2b372f5f3dc36f51383b3d6b5522757628f14200
ShinYoung-Kim <lukey0515@naver.com>
2024-01-06 19:33:08 +0900

    rename: 컴포넌트 폴더 이름 변경 반영

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
 import App from "./App.tsx";
-import { ProvidersTree } from "./component/ProviderTree.tsx";
+import { ProvidersTree } from "./components/ProviderTree.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
