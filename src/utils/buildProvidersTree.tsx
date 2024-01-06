fff383a29ff59718401b1d9aff9366a286be961c
ShinYoung-Kim <lukey0515@naver.com>
2024-01-06 19:48:23 +0900

    feat: Providers Tree 파일 분리

---
+++ src/utils/buildProvidersTree.tsx
@@ -0,0 +1,25 @@
+import { ComponentWithPropsArray } from "../types/providerTree";
+
+type Children = {
+	children: React.ReactNode;
+};
+
+export const buildProvidersTree = (
+	componentsWithProps: ComponentWithPropsArray
+) => {
+	const initialComponent = ({ children }: Children) => <>{children}</>;
+	return componentsWithProps.reduce(
+		(AccumulatedComponents, [Provider, props = {}]) => {
+			return ({ children }: Children) => {
+				return (
+					<AccumulatedComponents>
+						<Provider {...props}>
+							{children}
+						</Provider>
+					</AccumulatedComponents>
+				);
+			};
+		},
+		initialComponent
+	);
+};
