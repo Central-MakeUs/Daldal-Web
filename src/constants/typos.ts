efde093f016fbb054eccb0afa161e28e8cc9331b
ShinYoung-Kim <lukey0515@naver.com>
2024-01-08 00:47:41 +0900

    chore: konsta ui 설정 시도

---
+++ src/constants/typos.ts
@@ -0,0 +1,67 @@
+export const fontSizes = {
+	'.Headline': {
+		fontSize: '32px',
+	},
+	Subhead: '24px',
+	Body1: '18px',
+	Body2: '16px',
+	Body3: '14px',
+	Body4: '12px',
+	Caption1: '10px',
+	Caption2: '8px',
+	Caption3: '6px',
+	Btn_text1: '16px',
+};
+
+export const fontWeights = {
+	SB: '600',
+	M: '500',
+	R: '400',
+};
+
+export const fonts = {
+	'.Headline': {
+		// fontSize: '32px',
+		// fontWeight: '600',
+		'@apply text-[32px] font-[600]': {},
+	},
+	'.Subhead': {
+		fontSize: '24px',
+		fontWeight: '600',
+	},
+	'.Body1': {
+		fontSize: '18px',
+	},
+	'.Body2': {
+		fontSize: '16px',
+	},
+	'.Body3': {
+		fontSize: '14px',
+	},
+	'.Body4': {
+		fontSize: '12px',
+	},
+	'.Caption1': {
+		fontSize: '10px',
+	},
+	'.Caption2': {
+		fontSize: '8px',
+	},
+	'.Caption3': {
+		fontSize: '8px',
+	},
+	'.Btn_text1': {
+		fontSize: '16px',
+		fontWeight: '600',
+	},
+	'.SB': {
+		fontWeight: '600',
+	},
+	'.M': {
+		fontWeight: '500',
+		letterSpacing: '-0.25px',
+	},
+	'.R': {
+		fontWeight: '100',
+	},
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 import konstaConfig from 'konsta/config';
 import colors from './src/constants/colors';
+import { fonts } from './src/constants/typos';
+import plugin from 'tailwindcss/plugin';
 
 export default konstaConfig({
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
 	darkMode: 'class',
+	plugins: [
+		plugin(({ addComponents }) => {
+			addComponents(fonts);
+		}),
+	],
 	konsta: {
 		colors: colors,
-		fontFamily: { SUIT: ['SUIT', 'sans-serif'] },
+		component: plugin(({ addComponents }) => {
+			addComponents(fonts);
+		}),
+		fontSize: {
+			Headline: '3xl',
+		},
 	},
 	theme: {
-		colors: colors,
-		fontFamily: { SUIT: ['SUIT', 'sans-serif'] },
+		extends: {
+			colors: colors,
+			fontFamily: { SUIT: ['SUIT', 'sans-serif'] },
+			plugins: [
+				plugin(({ addComponents }) => {
+					addComponents(fonts);
+				}),
+			],
+			fontSize: {
+				Headline: '3xl',
+			},
+		},
 	},
-	plugins: [],
 });
