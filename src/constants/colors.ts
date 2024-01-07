54ebcd8133bd446f40b43134ae6858a29297bc70
coddingyun <igy2840@gmail.com>
2024-01-07 18:47:32 +0900

    design: color system 구축

---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+	Black: '#17181A',
+	White: '#FFFFFF',
+	Primary: '#FF617A',
+	'Secondary-B': '#75D6FF',
+	'Secondary-G': '#C1F78D',
+	Kakao: '#FBE44D',
+	Gray90: '#272A31',
+	Gray80: '#414651',
+	Gray70: '#5C6271',
+	Gray60: '#7C8393',
+	Gray50: '#979FB3',
+	Gray20: '#BBC2D2',
+	Gray10: '#D8DEED',
+};
+
+export default colors;
