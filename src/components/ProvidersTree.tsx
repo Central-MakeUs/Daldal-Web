2d809083e7d34066ee95ac28f195857c3a82737f
ShinYoung-Kim <lukey0515@naver.com>
2024-01-07 15:31:31 +0900

    feat: konsta ui provider 추가

--- src/components/ProvidersTree.tsx
+++ src/components/ProvidersTree.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { KonstaProvider } from "konsta/react";
 
 import { ComponentWithProps } from "../types/providerTree";
 import { buildProvidersTree } from "../utils/buildProvidersTree";
@@ -6,6 +7,7 @@ import { buildProvidersTree } from "../utils/buildProvidersTree";
 const queryClient = new QueryClient();
 const providerWithProps: ComponentWithProps[] = [
 	[QueryClientProvider, { client: queryClient }],
+	[KonstaProvider],
 ];
 
 export const ProvidersTree = buildProvidersTree(providerWithProps);
