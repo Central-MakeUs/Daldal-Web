39973e9b3239ccb04dc58d0f612152aadf14b071
ShinYoung-Kim <lukey0515@naver.com>
2024-01-07 16:28:14 +0900

    chore: 절대 경로 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,29 @@
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react()],
 	resolve: {
 		alias: [
-			{ find: '@', replacement: path.resolve(__dirname, 'src') },
+			{ find: '@', replacement: path.resolve(__dirname, './src') },
 			{
 				find: '@components',
-				replacement: path.resolve(__dirname, 'src/components'),
+				replacement: path.resolve(__dirname, './src/components'),
 			},
-			{ find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
-			{ find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-			{ find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
-			{ find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
-			{ find: '@types', replacement: path.resolve(__dirname, 'src/types') },
-			{ find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
-			{ find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+			{ find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
+			{ find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+			{ find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+			{ find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
+			{ find: '@types', replacement: path.resolve(__dirname, './src/types') },
+			{ find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+			{ find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
 			{
 				find: '@contants',
-				replacement: path.resolve(__dirname, 'src/contants'),
+				replacement: path.resolve(__dirname, './src/contants'),
 			},
-			{ find: '@stores', replacement: path.resolve(__dirname, 'src/stores') },
+			{ find: '@stores', replacement: path.resolve(__dirname, './src/stores') },
 		],
 	},
 });
