cc1298829305abc25eac7f8e11a67d091d0a6c3f
ShinYoung-Kim <lukey0515@naver.com>
2024-01-09 02:35:05 +0900

    Revert "Merge branch 'develop' of https://github.com/ShinYoung-Kim/Daldal-Web into feat#51.네비게이션_연결"
    This reverts commit 32a25d66c31c9f701adfc4132fb82dd0954795dc, reversing
    changes made to 5cf70dffbbc0251a92328da31df0def5d8b0076b.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import '@/index.css';
 import App from './App';
-import { ProvidersTree } from '@components/ProvidersTree';
+import { ProvidersTree } from '@components/ProvidersTree.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
