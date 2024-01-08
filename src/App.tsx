d4181011218e8a4d8c7dcf3d5d9b4482a62a3cdc
ShinYoung-Kim <lukey0515@naver.com>
2024-01-09 04:15:15 +0900

    feat: ionic 라우팅 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,67 @@
-import { Button } from 'konsta/react';
+import {
+	IonApp,
+	IonRouterOutlet,
+	setupIonicReact,
+	createAnimation,
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router-dom';
+import '@ionic/react/css/core.css';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import Hello from '@pages/Hello';
+import Home from '@pages/Home';
+
+setupIonicReact();
 
 function App() {
+	const animationBuilder = (
+		_baseEl: unknown,
+		opts: {
+			enteringEl: Element | Node | Element[] | Node[] | NodeList;
+			leavingEl: Element | Node | Element[] | Node[] | NodeList;
+		},
+	) => {
+		const enteringAnimation = createAnimation()
+			.addElement(opts.enteringEl)
+			.fromTo('opacity', 0, 1)
+			.duration(250);
+
+		const leavingAnimation = createAnimation()
+			.addElement(opts.leavingEl)
+			.fromTo('opacity', 1, 0)
+			.duration(250);
+
+		const animation = createAnimation()
+			.addAnimation(enteringAnimation)
+			.addAnimation(leavingAnimation);
+
+		return animation;
+	};
+
 	return (
-		<div className="">
-			<span className="Headline">안녕하세요</span>
-			<span className="R">안녕하세요</span>
-			<Button className="font-[600]">카카오톡으로 계속하기</Button>
-			<Button className="k-color-Kakao font-[100]">
-				카카오톡으로 계속하기
-			</Button>
-		</div>
+		<IonApp>
+			<IonReactRouter>
+				<IonRouterOutlet animation={animationBuilder}>
+					<Route exact path="/home">
+						<Home />
+					</Route>
+					<Route exact path="/">
+						<Redirect to="/home" />
+					</Route>
+				</IonRouterOutlet>
+			</IonReactRouter>
+		</IonApp>
 	);
 }
 
