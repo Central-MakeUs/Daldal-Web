e38f38e8554806cdf7d296643922a5bd8e30e460
ShinYoung-Kim <lukey0515@naver.com>
2024-01-08 17:48:05 +0900

    feat: BroswerRouter 추가

--- src/components/ProvidersTree.tsx
+++ src/components/ProvidersTree.tsx
@@ -3,11 +3,13 @@ import { KonstaProvider } from 'konsta/react';
 
 import { buildProvidersTree } from '@utils/buildProvidersTree';
 import { ComponentWithProps } from '@type/providerTree';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 const providerWithProps: ComponentWithProps[] = [
 	[QueryClientProvider, { client: queryClient }],
 	[KonstaProvider],
+	[BrowserRouter],
 ];
 
 export const ProvidersTree = buildProvidersTree(providerWithProps);
