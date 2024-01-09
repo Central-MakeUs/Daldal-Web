eb557c849a4d446d9d45f66ecf06430ff83dc7c6
ShinYoung-Kim <lukey0515@naver.com>
2024-01-09 21:38:24 +0900

    chore: ionic 스토리북 적용

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,22 @@
 import type { Preview } from '@storybook/react';
 
 import 'tailwindcss/tailwind.css';
+import { setupIonicReact } from '@ionic/react';
+import '@ionic/react/css/core.css';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+setupIonicReact();
 
 const preview: Preview = {
 	parameters: {
