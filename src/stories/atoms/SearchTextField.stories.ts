040f38c8332862f1f9336b470a7e13ca78c15f24
ShinYoung-Kim <lukey0515@naver.com>
2024-01-10 12:53:07 +0900

    feat: search text field 스토리 파일 생성

---
+++ src/stories/atoms/SearchTextField.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SearchTextField from '@components/atoms/SearchTextField';
+
+const meta = {
+	title: 'Atoms/SearchTextField',
+	component: SearchTextField,
+	parameters: {
+		// More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+		layout: 'centered',
+	},
+} satisfies Meta<typeof SearchTextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
