51629d5c8b54d8c08fde0ed2e53baea707aae6ab
ShinYoung-Kim <lukey0515@naver.com>
2024-01-10 12:52:54 +0900

    feat: search text field 컴포넌트 생성

---
+++ src/components/atoms/SearchTextField.tsx
@@ -0,0 +1,17 @@
+import SvgIcon from '@assets/icons/SvgIcon';
+import colors from '@constants/colors';
+
+function SearchTextField() {
+	return (
+		<div className="bg-Gray80 rounded-[100px] w-full flex px-4 py-[6px] gap-2 justify-center items-center">
+			<SvgIcon id="search" size={24} color={colors.Gray20} />
+			<input
+				type="text"
+				placeholder="Search"
+				className="bg-Gray80 text-white placeholder:text-Gray20 flex-1 focus-visible:outline-none"
+			/>
+		</div>
+	);
+}
+
+export default SearchTextField;
