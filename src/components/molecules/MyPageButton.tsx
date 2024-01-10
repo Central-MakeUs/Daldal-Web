c3c42138fc6d8836cd582c06ad2af03b870417be
coddingyun <igy2840@gmail.com>
2024-01-10 21:34:49 +0900

    chore: 컴포넌트 type 작성 방식 변경

--- src/components/molecules/MyPageButton.tsx
+++ src/components/molecules/MyPageButton.tsx
@@ -2,7 +2,7 @@ import SvgIcon from '@assets/icons/SvgIcon';
 import IconButton from '@components/atoms/IconButton';
 import { moleculeIconButton } from '@type/moleculeIconButton';
 
-const MyPageButton = ({ onClick }: moleculeIconButton) => {
+const MyPageButton: React.FC<moleculeIconButton> = ({ onClick }) => {
 	return (
 		<IconButton width={12} size="large" bgColor="Black" onClick={onClick}>
 			<SvgIcon id="mypage-person" size={32} />
--- src/components/molecules/PointButton.tsx
+++ src/components/molecules/PointButton.tsx
@@ -2,7 +2,7 @@ import SvgIcon from '@assets/icons/SvgIcon';
 import IconButton from '@components/atoms/IconButton';
 import { moleculeIconButton } from '@type/moleculeIconButton';
 
-const PointButton = ({ onClick }: moleculeIconButton) => {
+const PointButton: React.FC<moleculeIconButton> = ({ onClick }) => {
 	return (
 		<IconButton width={12} size="large" bgColor="Black" onClick={onClick}>
 			<SvgIcon id="point-paid" size={32} />
