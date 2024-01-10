10fbfa8cdfaa4b5e229199a9b9d2559a02c13bde
ShinYoung-Kim <lukey0515@naver.com>
2024-01-10 12:10:18 +0900

    feat: app에서 라우팅 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Route } from 'react-router-dom';
+import { IonApp, setupIonicReact } from '@ionic/react';
+
 import '@ionic/react/css/core.css';
-/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-import Home from '@pages/Home';
-import { buildAnimation } from '@utils/buildAnimation';
+
+import Routing from '@components/Routing';
 
 setupIonicReact();
 
 function App() {
 	return (
 		<IonApp>
-			<IonReactRouter>
-				<IonRouterOutlet animation={buildAnimation}>
-					<Route path="/home" component={Home} exact={true} />
-				</IonRouterOutlet>
-			</IonReactRouter>
+			<Routing />
 		</IonApp>
 	);
 }
