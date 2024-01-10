f3bce8d2ed8fa5843070d403a64ed3e86dcbd6cb
coddingyun <igy2840@gmail.com>
2024-01-10 22:13:16 +0900

    fix: SvgIcon 직사각형일 경우 width, height / 정사각형일 경우 size 사용

--- src/assets/icons/SvgIcon.tsx
+++ src/assets/icons/SvgIcon.tsx
@@ -1,8 +1,8 @@
 import iconsSvg from '@assets/icons/icons.svg';
 import svgIcon from '@type/svgIcon';
 
-const SvgIcon = ({ id, color, size }: svgIcon) => (
-	<svg fill={color} width={size} height={size}>
+const SvgIcon = ({ id, color, size, width, height }: svgIcon) => (
+	<svg fill={color} width={size || width} height={size || height}>
 		<use href={`${iconsSvg}#${id}`} />
 	</svg>
 );
