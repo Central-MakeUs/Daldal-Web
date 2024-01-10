a1d610b1b67d562e499efdeb240065b32c896922
ShinYoung-Kim <lukey0515@naver.com>
2024-01-11 05:21:40 +0900

    test: list point storybook 추가

---
+++ src/stories/atoms/ListPoint.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ListPoint from '@components/atoms/ListPoint';
+
+const meta = {
+	title: 'components/atoms/ListPoint',
+	component: ListPoint,
+	parameters: {
+		layout: 'centerd',
+	},
+	tags: ['autodocs'],
+	argTypes: {
+		date: {
+			control: {
+				type: 'text',
+			},
+		},
+	},
+} satisfies Meta<typeof ListPoint>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		date: '2023.12.23',
+		point: 200,
+	},
+};
