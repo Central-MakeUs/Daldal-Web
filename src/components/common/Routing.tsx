40c984210495a6146250ffefa320109980697a03
ShinYoung-Kim <lukey0515@naver.com>
2024-01-11 01:05:47 +0900

    rename: 컴포넌트 내 common 폴더 생성

---
+++ src/components/common/Routing.tsx
@@ -0,0 +1,21 @@
+import { IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from '@pages/Home';
+
+import { buildAnimation } from '@utils/buildAnimation';
+
+function Routing() {
+	return (
+		// <IonReactRouter>
+		// <IonRouterOutlet animation={buildAnimation}>
+		<BrowserRouter>
+			<Route path="/home" component={Home} exact={true} />
+		</BrowserRouter>
+		// </IonRouterOutlet>
+		// </IonReactRouter>
+	);
+}
+
+export default Routing;
