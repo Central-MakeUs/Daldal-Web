e772a4ba4e376d93864ffd6c2dcc599b1ffde925
ShinYoung-Kim <lukey0515@naver.com>
2024-01-11 03:16:04 +0900

    feat: icon id type 생성

--- src/type/svgIcon.ts
+++ src/type/svgIcon.ts
@@ -1,5 +1,27 @@
+type IconId =
+	| 'app-logo'
+	| 'direction-left'
+	| 'direction-right'
+	| 'like-full'
+	| 'like-with-num'
+	| 'like-empty'
+	| 'check'
+	| 'circle-check'
+	| 'mypage-person'
+	| 'point-paid'
+	| 'search'
+	| 'star'
+	| 'category-bags'
+	| 'category-hat'
+	| 'category-food'
+	| 'category-clothes'
+	| 'category-jewlery'
+	| 'category-makeup'
+	| 'category-living'
+	| 'category-shoes';
+
 type svgIcon = {
-	id: string;
+	id: IconId;
 	color?: string;
 	size?: number;
 };
