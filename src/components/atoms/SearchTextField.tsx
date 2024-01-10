42a228f7eee755513468e894ff58a3d010595d7e
ShinYoung-Kim <lukey0515@naver.com>
2024-01-10 13:02:13 +0900

    feat: search text field 값 다루는 함수 추가

--- src/components/atoms/SearchTextField.tsx
+++ src/components/atoms/SearchTextField.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 import SvgIcon from '@assets/icons/SvgIcon';
 import colors from '@constants/colors';
 
 function SearchTextField() {
+	const [searchValue, setSearchValue] = useState('');
+
+	const handleClearSearchValue = () => {
+		setSearchValue('');
+	};
+
+	const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			handleClearSearchValue();
+		}
+	};
+
 	return (
 		<div className="bg-Gray80 rounded-[100px] w-full flex px-4 py-[6px] gap-2 justify-center items-center">
 			<SvgIcon id="search" size={24} color={colors.Gray20} />
 			<input
+				value={searchValue}
+				onChange={e => setSearchValue(e.target.value)}
+				onKeyDown={handleEnter}
 				type="text"
 				placeholder="Search"
 				className="bg-Gray80 text-white placeholder:text-Gray20 flex-1 focus-visible:outline-none typography-Body2 typography-R"
