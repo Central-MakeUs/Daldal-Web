5125a659dca68ec76dbbb8902d97c3a04c27f128
ShinYoung-Kim <lukey0515@naver.com>
2024-01-12 10:34:47 +0900

    feat: 불필요한 div 태그 제거

--- src/components/atoms/MyPage/CategoryButton.tsx
+++ src/components/atoms/MyPage/CategoryButton.tsx
@@ -14,16 +14,12 @@ function CategoryButton({ iconId, title, onClick }: CategoryButtonProps) {
 	return (
 		<Button
 			onClick={onClick}
-			className={`px-4 py-[15px] k-color-Gray80 text-White rounded-xl !typography-Body1 !typography-R box-content`}
+			className={`px-4 py-[15px] k-color-Gray80 text-White rounded-xl !typography-Body1 !typography-R box-content ${
+				iconId ? '!justify-start' : '!justify-center'
+			} items-center w-full gap-2`}
 		>
-			<div
-				className={`flex justify-${
-					iconId ? 'start' : 'center'
-				} gap-2 w-full items-center`}
-			>
-				{iconId && <SvgIcon id={iconId} color={colors.White} size={24} />}
-				{title}
-			</div>
+			{iconId && <SvgIcon id={iconId} color={colors.White} size={24} />}
+			{title}
 		</Button>
 	);
 }
