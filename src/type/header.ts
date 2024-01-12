1f3f98f7400585120b2875a4e8c8b44c9a918ab8
coddingyun <igy2840@gmail.com>
2024-01-12 19:38:21 +0900

    chore: 기존 파일 삭제

--- src/type/header.ts
+++
@@ -1,4 +0,0 @@
-export type header = {
-	leftHeader: React.ReactNode;
-	rightHeader: React.ReactNode;
-};
--- src/type/moleculeIconButton.ts
+++
@@ -1,7 +0,0 @@
-export interface moleculeIconButton {
-	onClick: () => void;
-}
-
-export interface moleculeIconButtonWithColor extends moleculeIconButton {
-	color: string;
-}
