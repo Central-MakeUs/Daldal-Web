c28e53e8a4cb266abd7239de07cfa55fc3e59f01
ShinYoung-Kim <lukey0515@naver.com>
2024-01-14 22:55:36 +0900

    fix: 잘못된 유효성 검사 제거

--- src/type/form.ts
+++ src/type/form.ts
@@ -10,10 +10,8 @@ export const schema = z.object({
 			required_error: '필수 입력 사항입니다.',
 		})
 		.regex(/^[0-9]+$/, '숫자만 입력해주세요.')
-		.max(12, '16 글자 이하로 입력해주세요.'),
-	POINT: z
-		.string({ required_error: '필수 입력 사항입니다.' })
-		.max(12, '12 글자 이하로 입력해주세요.'),
+		.max(16, '16 글자 이하로 입력해주세요.'),
+	POINT: z.string({ required_error: '필수 입력 사항입니다.' }),
 });
 
 export type FormType = z.infer<typeof schema>;
