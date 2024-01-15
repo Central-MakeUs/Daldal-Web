e826a438f08b85f6daa1ad1a07383867233dd4eb
ShinYoung-Kim <lukey0515@naver.com>
2024-01-15 16:22:24 +0900

    feat: routing에 point 추가

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from '@pages/Home';
 import MyPage from '@pages/MyPage';
+import Point from '@pages/Point';
 
 function Routing() {
 	return (
 		<BrowserRouter>
-			<Route path="/home" component={Home} exact={true} />
 			<Route path="/" component={Home} exact />
+			<Route path="/home" component={Home} exact />
 			<Route path="/my-page" component={MyPage} exact />
+			<Route path="/point" component={Point} exact />
 		</BrowserRouter>
 	);
 }
