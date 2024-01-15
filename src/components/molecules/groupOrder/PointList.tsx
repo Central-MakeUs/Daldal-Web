23065bd449fa7db3eb8aaa85771e8c61406d7a3b
ShinYoung-Kim <lukey0515@naver.com>
2024-01-16 02:19:30 +0900

    chore: 폴더 이름 대소문자 변경 미반영 수정

---
+++ src/components/molecules/groupOrder/PointList.tsx
@@ -0,0 +1,50 @@
+import ListPoint from '@components/atoms/ListPoint';
+import {
+	GROUP_ORDER_LIST_POINT_DESCRIPTION,
+	POINT_LIST_TITLE_BY_MONTH,
+	PointListTitleByMonthType,
+} from '@constants/text';
+import { Point } from '@models/point/entity/point';
+import { getPointText } from '@utils/formatData';
+
+type PointListProps = {
+	totalPoint: number;
+	pointList: Point[];
+	month: PointListTitleByMonthType;
+};
+
+const PointList = ({ totalPoint, pointList, month }: PointListProps) => {
+	const descriptions = GROUP_ORDER_LIST_POINT_DESCRIPTION;
+	const isThisMonth = month === 'THIS';
+
+	return (
+		<div className="flex flex-col gap-6 bg-Gray80 p-4 rounded-[13px]">
+			<div className="flex flex-col gap-3">
+				<span className="typography-Body2 typography-SB text-White">
+					{POINT_LIST_TITLE_BY_MONTH[month]}
+				</span>
+				<div className="flex flex-col typography-Body4 typography-R text-Gray20">
+					{isThisMonth &&
+						descriptions.map((description, index) => (
+							<span key={`PointListDescription#${index}`}>{description}</span>
+						))}
+				</div>
+			</div>
+			<span className="typography-Headline text-White self-end">
+				{getPointText(totalPoint)}
+			</span>
+			<div className="flex flex-col gap-2">
+				{pointList.map(({ point, date }, index) => (
+					<ListPoint
+						key={`ListPoint#${index}`}
+						point={point}
+						date={date}
+						onClick={() => console.log('포인트 정보 상세보기로 이동')}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default PointList;
---
+++ src/stories/molecules/groupOrder/PointList.stories.tsx
@@ -0,0 +1,33 @@
+import PointList from '@components/molecules/groupOrder/PointList';
+import { NEXT_MONTH_POINT_LIST, THIS_MONTH_POINT_LIST } from '@mocks/pointList';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+	title: 'molecules/groupOrder/PointList',
+	component: PointList,
+	decorators: [
+		Story => (
+			<div style={{ width: '375px' }}>
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		layout: 'centered',
+		backgrounds: {
+			default: 'dark',
+		},
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof PointList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ThisMonth: Story = {
+	args: THIS_MONTH_POINT_LIST,
+};
+
+export const NextMonth: Story = {
+	args: NEXT_MONTH_POINT_LIST,
+};
