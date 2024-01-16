e48971bd5990c418420a3beb5641659c9bea4801
ShinYoung-Kim <lukey0515@naver.com>
2024-01-17 04:45:45 +0900

    feat: product card list 확장성 있게 변경

--- src/components/organisms/ProductCardList.tsx
+++ src/components/organisms/ProductCardList.tsx
@@ -1,15 +1,29 @@
 import ProductCard from '@components/molecules/ProductCard';
 import { mockProductList } from '@mocks/productList';
 
-const ProductCardList = () => {
+type ProductCardListProps = {
+	gridCols?: number;
+	size?: 'small' | 'medium' | 'large';
+	type?: 'heart' | 'checkbox' | 'default';
+};
+
+const ProductCardList = ({
+	gridCols,
+	size = 'medium',
+	type = 'default',
+}: ProductCardListProps) => {
+	const classNameOfGridCols = gridCols
+		? `grid-cols-${gridCols}`
+		: 'grid-cols-2';
+
 	return (
-		<div className="grid grid-cols-2 gap-4">
+		<div className={`grid gap-4 ${classNameOfGridCols}`}>
 			{mockProductList.map(product => {
 				return (
 					<ProductCard
 						thumbnail={product.image}
-						size="medium"
-						type="heart"
+						size={size}
+						type={type}
 						key={product.id}
 						{...product}
 					/>
