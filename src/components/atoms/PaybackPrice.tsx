e3917f3ef62ced35ac98d230b007602f41f89aed
ShinYoung-Kim <lukey0515@naver.com>
2024-01-16 21:28:38 +0900

    design: payback price 컴포넌트 디자인 적용

--- src/components/atoms/PaybackPrice.tsx
+++ src/components/atoms/PaybackPrice.tsx
@@ -6,10 +6,17 @@ type PaybackPriceProps = {
 };
 
 const PaybackPrice = ({ size, price }: PaybackPriceProps) => {
+	const classNameBySize = {
+		small: 'typography-Body4 typography-M gap-1',
+		medium: 'typography-Body2 typography-M gap-2',
+		large: 'typography-Body1 typography-SB gap-2',
+	};
 	return (
-		<div>
-			<span>예상 환금액</span>
-			<span>{getPriceText(price)}</span>
+		<div className={`flex items-center ${classNameBySize[size]} text-Primary`}>
+			<span className="typography-Caption1 text-White bg-Primary px-1 py-[2px] rounded-sm">
+				예상 환금액
+			</span>
+			<span className={`text-Primary`}>{getPriceText(price)}</span>
 		</div>
 	);
 };
