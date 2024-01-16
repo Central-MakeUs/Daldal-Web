f14bb84666977295009a620bf354270b623c4d3e
ShinYoung-Kim <lukey0515@naver.com>
2024-01-17 02:38:40 +0900

    feat: backdrop filter 컴포넌트 생성

---
+++ src/stories/atoms/BackdropFilter.stories.tsx
@@ -0,0 +1,21 @@
+import BackdropFilter from '@components/atoms/BackdropFilter';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+	title: 'atoms/BackdropFilter',
+	component: BackdropFilter,
+	parameters: {
+		layout: 'centerd',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof BackdropFilter>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		isOverlayNeeded: true,
+	},
+};
