9677defa45eaffe782f905e106e04772e9a9cdaf
ShinYoung-Kim <lukey0515@naver.com>
2024-01-16 20:45:01 +0900

    style: 불필요한 id 제거

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -14,10 +14,7 @@ const Routing = () => {
 			<Route path="/home" component={Home} exact />
 			<Route path="/my-page" component={MyPage} exact />
 			<Route path="/point" component={Point} exact />
-			<div
-				className="w-full max-w-[600px] fixed bottom-0"
-				id="bottomsheet 자리"
-			>
+			<div className="w-full max-w-[600px] fixed bottom-0">
 				{isBottomSheetOpen && <LoginBottomSheet />}
 			</div>
 		</BrowserRouter>
