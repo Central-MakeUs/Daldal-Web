0686cb5a2abf5ad51a3db6ebf9fc59679422347c
coddingyun <igy2840@gmail.com>
2024-01-17 03:27:31 +0900

    feat: BankButton 컴포넌트 스토리  생성

---
+++ src/stories/atoms/myPage/BankButton.stories.tsx
@@ -0,0 +1,31 @@
+import BankButton from '@components/atoms/myPage/BankButton';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+	title: 'atoms/myPage/BankButton',
+	component: BankButton,
+	decorators: [
+		Story => (
+			<div style={{ width: '375px' }}>
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		layout: 'centered',
+		backgrounds: {
+			default: 'dark',
+		},
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof BankButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Test: Story = {
+	args: {
+		iconId: 'like-full',
+		title: 'NH 농협',
+	},
+};
