b750798c36aaea3f5bafcb8f9d659484ee7cfc17
ShinYoung-Kim <lukey0515@naver.com>
2024-01-17 05:34:10 +0900

    feat: 상품 카테고리 버튼 목록 추가

---
+++ src/components/molecules/CategoryButtonList.tsx
@@ -0,0 +1,34 @@
+import ProductCategoryButton from '@components/atoms/button/ProductCategoryButton';
+import { IconId } from '@type/svgIcon';
+
+const CategoryButtonList = () => {
+	const categoyList: {
+		id: IconId;
+		name: string;
+	}[] = [
+		{ id: 'category-clothes', name: '의류' },
+		{ id: 'category-hat', name: '모자' },
+		{ id: 'category-bags', name: '가방' },
+		{ id: 'category-shoes', name: '신발' },
+		{ id: 'category-jewlery', name: '쥬얼리' },
+	];
+
+	return (
+		<div className="flex justify-between py-3 px-7 my-2">
+			{categoyList.map(category => {
+				return (
+					<ProductCategoryButton
+						key={category.id}
+						category={category.name}
+						handleCategoryButtonClick={() => {
+							console.log(category.name + '카테고리로 이동');
+						}}
+						iconId={category.id}
+					/>
+				);
+			})}
+		</div>
+	);
+};
+
+export default CategoryButtonList;
