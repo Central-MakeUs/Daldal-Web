5c2397ad9f556661df5648849741a0fd376a86fb
ShinYoung-Kim <lukey0515@naver.com>
2024-01-16 19:42:49 +0900

    chore: shadcn 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import konstaConfig from 'konsta/config';
+import plugin from 'tailwindcss/plugin';
+
+import tailwindAnimate from 'tailwindcss-animate';
+
 import colors from './src/constants/colors';
 import { fonts } from './src/constants/typos';
-import plugin from 'tailwindcss/plugin';
 
 export default konstaConfig({
 	content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
@@ -11,6 +14,7 @@ export default konstaConfig({
 		plugin(({ addComponents }) => {
 			addComponents(fonts);
 		}),
+		tailwindAnimate,
 	],
 	konsta: {
 		colors: colors,
@@ -19,6 +23,21 @@ export default konstaConfig({
 		extends: {
 			colors: colors,
 			fontFamily: { SUIT: ['SUIT', 'sans-serif'] },
+
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
 		},
 	},
 });
