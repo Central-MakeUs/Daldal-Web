00af99c6f0ad4fd312263c3f58267b01db93f76b
ShinYoung-Kim <lukey0515@naver.com>
2024-01-16 19:45:35 +0900

    feat: 라우팅 파일에 login bottom sheet 자리 생성

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -1,15 +1,25 @@
+import LoginBottomSheet from '@components/molecules/LoginBottomSheet';
 import Home from '@pages/Home';
 import MyPage from '@pages/MyPage';
 import Point from '@pages/Point';
+import { useBottomSheetStore } from '@stores/layerStore';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Routing = () => {
+	const { isBottomSheetOpen } = useBottomSheetStore();
+
 	return (
 		<BrowserRouter>
 			<Route path="/" component={Home} exact />
 			<Route path="/home" component={Home} exact />
 			<Route path="/my-page" component={MyPage} exact />
 			<Route path="/point" component={Point} exact />
+			<div
+				className="w-full max-w-[600px] h-[250px] fixed bottom-0"
+				id="bottomsheet 자리"
+			>
+				{isBottomSheetOpen && <LoginBottomSheet />}
+			</div>
 		</BrowserRouter>
 	);
 };
