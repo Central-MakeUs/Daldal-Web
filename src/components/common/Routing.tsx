4c620413acfbfde997100ccb567da00647d61199
coddingyun <igy2840@gmail.com>
2024-01-17 02:57:33 +0900

    chore: 예시 코드 추가

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -1,12 +1,21 @@
+import { Button } from 'konsta/react';
+
+import CustomToaster from '@components/atoms/CustomToaster';
+import ToastMessageLikeOrDelete from '@components/atoms/ToastMessageLikeOrDelete';
 import LoginBottomSheet from '@components/molecules/LoginBottomSheet';
 import Home from '@pages/Home';
 import MyPage from '@pages/MyPage';
 import Point from '@pages/Point';
 import { useBottomSheetStore } from '@stores/layerStore';
+import toast from 'react-hot-toast';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Routing = () => {
 	const { isBottomSheetOpen } = useBottomSheetStore();
+
+	const handleClick = () => {
+		toast(() => <ToastMessageLikeOrDelete like={true} />);
+	};
 
 	return (
 		<BrowserRouter>
@@ -16,6 +25,8 @@ const Routing = () => {
 			<Route path="/point" component={Point} exact />
 			<div className="w-full max-w-[600px] fixed bottom-0">
 				{isBottomSheetOpen && <LoginBottomSheet />}
+				<CustomToaster className="px-3" />
+				<Button onClick={handleClick}>toast 생성</Button>
 			</div>
 		</BrowserRouter>
 	);
