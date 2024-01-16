42d2b92bf098e7dd2c9d7a875e2ac1bf19648c74
ShinYoung-Kim <lukey0515@naver.com>
2024-01-17 05:35:38 +0900

    feat: home 페이지에 레이아웃 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
+import SearchTextField from '@components/atoms/SearchTextField';
+import CategoryButtonList from '@components/molecules/CategoryButtonList';
+import ProductCardList from '@components/organisms/ProductCardList';
+import RecommendationProductCardList from '@components/organisms/RecommendationProductCardList';
 import PageLayout from '@components/templates/PageLayout';
 
 const Home = () => {
 	return (
 		<PageLayout leftType="logo" className="relative">
-			홈 화면
+			<SearchTextField />
+			<RecommendationProductCardList />
+			<CategoryButtonList />
+			<div className="my-3">
+				<ProductCardList type="heart" />
+			</div>
 		</PageLayout>
 	);
 };
