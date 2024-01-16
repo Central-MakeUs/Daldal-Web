5a7e5b5c57d4289cc3a7d6ec7a61a9ef45606140
coddingyun <igy2840@gmail.com>
2024-01-17 03:29:10 +0900

    design: padding 0 적용

--- src/components/atoms/myPage/BankButton.tsx
+++ src/components/atoms/myPage/BankButton.tsx
@@ -10,7 +10,7 @@ type BankButtonProps = {
 
 const BankButton = ({ iconId, title }: BankButtonProps) => {
 	return (
-		<Button className="h-[72px] flex flex-col gap-1 justify-center bg-transparent active:bg-transparent">
+		<Button className="h-[72px] flex flex-col gap-1 justify-center !p-0 bg-transparent active:bg-transparent">
 			<SvgIcon id={iconId} size={40} />
 			<h3 className="typography-Body4 typography-M text-Black">{title}</h3>
 		</Button>
