77431ef6ceda1db03cfee84e7457f33be6e48ce5
coddingyun <igy2840@gmail.com>
2024-01-17 23:49:11 +0900

    feat: ImageUploadButton 컨테이너 전체가 버튼이 되도록 변경

--- src/components/atoms/ImageUploadButton.tsx
+++ src/components/atoms/ImageUploadButton.tsx
@@ -1,19 +1,18 @@
-import IconButton from '@components/atoms/button/IconButton';
+import { Button } from 'konsta/react';
+
+import SvgIcon from '@components/common/SvgIcon';
 
 const ImageUploadButton = () => {
 	const handleClick = () => {
 		console.log('갤러리/폴더 열기');
 	};
 	return (
-		<div className="bg-Gray80 rounded-[5px] relative flex items-center justify-center aspect-square">
-			<IconButton
-				icon={{
-					id: 'plus',
-					size: 32,
-				}}
-				onClick={handleClick}
-			/>
-		</div>
+		<Button
+			className="k-color-Gray80 rounded-[5px] h-full !relative !flex !items-center !justify-center !aspect-square"
+			onClick={handleClick}
+		>
+			<SvgIcon id="plus" size={32} />
+		</Button>
 	);
 };
 
