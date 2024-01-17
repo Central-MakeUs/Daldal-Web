5356589a2776400d801e0b0bf3b201f13b00d639
coddingyun <igy2840@gmail.com>
2024-01-18 00:22:38 +0900

    refactor: 토스트 생성 버튼 제거

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -1,21 +1,13 @@
-import { Button } from 'konsta/react';
-
-import ToastMessageLikeOrDelete from '@components/atoms/ToastMessageLikeOrDelete';
 import LoginBottomSheet from '@components/molecules/LoginBottomSheet';
 import Home from '@pages/Home';
 import ImageUpload from '@pages/ImageUpload';
 import MyPage from '@pages/MyPage';
 import Point from '@pages/Point';
 import { useBottomSheetStore } from '@stores/layerStore';
-import toast from 'react-hot-toast';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const Routing = () => {
 	const { isBottomSheetOpen } = useBottomSheetStore();
-
-	const handleClick = () => {
-		toast(() => <ToastMessageLikeOrDelete like={true} />);
-	};
 
 	return (
 		<BrowserRouter>
@@ -26,7 +18,6 @@ const Routing = () => {
 			<Route path="/image-upload" component={ImageUpload} exact />
 			<div className="w-full max-w-[600px] fixed bottom-0">
 				{isBottomSheetOpen && <LoginBottomSheet />}
-				<Button onClick={handleClick}>toast 생성</Button>
 			</div>
 		</BrowserRouter>
 	);
