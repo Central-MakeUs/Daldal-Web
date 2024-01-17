1a4d08ba37b29c1dccf5fc2624e7dd1878c8f2ba
coddingyun <igy2840@gmail.com>
2024-01-17 23:50:23 +0900

    feat: ImageUpload Page onClick 속성 추가

--- src/pages/ImageUpload.tsx
+++ src/pages/ImageUpload.tsx
@@ -11,22 +11,27 @@ const ImageUpload = () => {
 					<ImageContainer
 						imageUrl="https://via.placeholder.com/2560x1440"
 						status="APPROVED"
+						onClick={() => console.log('test')}
 					/>
 					<ImageContainer
 						imageUrl="https://via.placeholder.com/2560x1440"
 						status="APPROVED"
+						onClick={() => console.log('test')}
 					/>
 					<ImageContainer
 						imageUrl="https://via.placeholder.com/2560x1440"
 						status="NOT_APPROVED"
+						onClick={() => console.log('test')}
 					/>
 					<ImageContainer
 						imageUrl="https://via.placeholder.com/2560x1440"
 						status="PROGRESS"
+						onClick={() => console.log('test')}
 					/>
 					<ImageContainer
 						imageUrl="https://via.placeholder.com/2560x1440"
 						status="NOT_APPROVED"
+						onClick={() => console.log('test')}
 					/>
 				</div>
 			</PageLayout>
