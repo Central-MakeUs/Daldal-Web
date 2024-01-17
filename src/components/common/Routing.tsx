b3f85639f27d490b792d06d2bb4f79f94743ec1a
coddingyun <igy2840@gmail.com>
2024-01-17 21:59:27 +0900

    feat: page 생성 및 라우팅 연결

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -3,6 +3,7 @@ import { Button } from 'konsta/react';
 import ToastMessageLikeOrDelete from '@components/atoms/ToastMessageLikeOrDelete';
 import LoginBottomSheet from '@components/molecules/LoginBottomSheet';
 import Home from '@pages/Home';
+import ImageUpload from '@pages/ImageUpload';
 import MyPage from '@pages/MyPage';
 import Point from '@pages/Point';
 import { useBottomSheetStore } from '@stores/layerStore';
@@ -22,6 +23,7 @@ const Routing = () => {
 			<Route path="/home" component={Home} exact />
 			<Route path="/my-page" component={MyPage} exact />
 			<Route path="/point" component={Point} exact />
+			<Route path="/image-upload" component={ImageUpload} exact />
 			<div className="w-full max-w-[600px] fixed bottom-0">
 				{isBottomSheetOpen && <LoginBottomSheet />}
 				<Button onClick={handleClick}>toast 생성</Button>
