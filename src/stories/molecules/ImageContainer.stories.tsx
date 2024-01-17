538f7ac9de248369d5c77e222b988b32cf7289e0
coddingyun <igy2840@gmail.com>
2024-01-17 23:58:07 +0900

    feat: ImageContainer 컴포넌트 스토리 생성

---
+++ src/stories/molecules/ImageContainer.stories.tsx
@@ -0,0 +1,45 @@
+import ImageContainer from '@components/molecules/ImageContainer';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+	title: 'molecules/ImageContainer',
+	component: ImageContainer,
+	decorators: [
+		Story => (
+			<div style={{ width: '101px' }}>
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		layout: 'centered',
+		backgrounds: {
+			default: 'dark',
+		},
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof ImageContainer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Approved: Story = {
+	args: {
+		imageUrl: 'https://via.placeholder.com/2560x1440',
+		status: 'APPROVED',
+	},
+};
+
+export const Progress: Story = {
+	args: {
+		imageUrl: 'https://via.placeholder.com/2560x1440',
+		status: 'PROGRESS',
+	},
+};
+
+export const NOT_APPROVED: Story = {
+	args: {
+		imageUrl: 'https://via.placeholder.com/2560x1440',
+		status: 'NOT_APPROVED',
+	},
+};
