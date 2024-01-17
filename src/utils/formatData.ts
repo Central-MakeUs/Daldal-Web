7bd99e9f2c9d81f41731403febcd5d2fecc9c978
ShinYoung-Kim <lukey0515@naver.com>
2024-01-17 10:59:15 +0900

    feat: 포인트 연산 함수 변경

--- src/utils/formatData.ts
+++ src/utils/formatData.ts
@@ -1,9 +1,19 @@
+import { Point } from '@models/point/entity/point';
+
 export const changeNumberIntoStringWithComma = (point: number) => {
 	return new Intl.NumberFormat().format(point);
 };
 
-export const getPointText = (point: number) => {
-	return `${changeNumberIntoStringWithComma(point)} P`;
+export const isPointState = (point: Point['point']) => {
+	return !/^[0-9]+$/.test(String(point));
+};
+
+export const getPointText = (point: Point['point']) => {
+	if (isPointState(point)) {
+		return point;
+	}
+
+	return `${changeNumberIntoStringWithComma(+point)} P`;
 };
 
 export const getOriginalPoint = (pointText: string) => {
