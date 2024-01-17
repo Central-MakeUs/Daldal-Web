000bb00248e64a063d380df00cb8d73050a57efb
coddingyun <igy2840@gmail.com>
2024-01-17 22:08:48 +0900

    feat: ImageUpload page 생성 및 컴포넌트 확인

--- src/pages/ImageUpload.tsx
+++ src/pages/ImageUpload.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import UploadButton from '@components/atoms/imageUpload/UploadButton';
+import PageLayout from '@components/templates/PageLayout';
 
 const ImageUpload = () => {
-	return <div>ImageUpload</div>;
+	return (
+		<div>
+			<PageLayout leftType="back">
+				<div className="grid grid-cols-3 gap-[11px]">
+					<UploadButton />
+					<UploadButton />
+					<UploadButton />
+				</div>
+			</PageLayout>
+		</div>
+	);
 };
 
 export default ImageUpload;
