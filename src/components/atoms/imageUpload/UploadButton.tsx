3e923060c3f734551f1a176bc6b4f78489fed90d
coddingyun <igy2840@gmail.com>
2024-01-17 22:08:13 +0900

    design: 업로드 버튼 배경 색 변경

--- src/components/atoms/imageUpload/UploadButton.tsx
+++ src/components/atoms/imageUpload/UploadButton.tsx
@@ -5,7 +5,7 @@ const UploadButton = () => {
 		console.log('갤러리/폴더 열기');
 	};
 	return (
-		<div className="bg-Gray50 rounded-[5px] relative flex items-center justify-center aspect-square">
+		<div className="bg-Gray80 rounded-[5px] relative flex items-center justify-center aspect-square">
 			<IconButton
 				icon={{
 					id: 'plus',
