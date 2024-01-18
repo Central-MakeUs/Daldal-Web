fdd69571559506c8129a204113931ce7bf27b74e
coddingyun <igy2840@gmail.com>
2024-01-18 23:59:38 +0900

    design: AccountBottomSheet x버튼 위치 변경

--- src/components/molecules/bottomSheet/AccountBottomSheet.tsx
+++ src/components/molecules/bottomSheet/AccountBottomSheet.tsx
@@ -14,24 +14,22 @@ const AccountBottomSheet = () => {
 	return (
 		<Drawer open={isBottomSheetOpen} onOpenChange={setBottomSheetOpen}>
 			<DrawerContent className="mx-auto w-full max-w-[600px] px-6 !h-[206px]">
-				<div className="w-full flex justify-between items-start">
-					<div>
-						<h4 className="text-Gray60 typography-Body4 typography-M leading-normal mb-2">
-							계좌가 등록되어 있지 않아, <br /> 달달한 포인트를 받기 어려워요 😥
-						</h4>
-						<h2 className="text-Black typography-Body1 typography-M">
-							출금 계좌를 등록해주세요!
-						</h2>
-					</div>
-					<IconButton
-						onClick={closeBottomSheet}
-						icon={{
-							id: 'close',
-							color: colors.Gray60,
-							size: 24,
-						}}
-						className="!w-fit"
-					/>
+				<IconButton
+					onClick={closeBottomSheet}
+					icon={{
+						id: 'close',
+						color: colors.Gray60,
+						size: 24,
+					}}
+					className="!w-fit absolute top-1 right-0"
+				/>
+				<div>
+					<h4 className="text-Gray60 typography-Body4 typography-M leading-normal mb-2">
+						계좌가 등록되어 있지 않아, <br /> 달달한 포인트를 받기 어려워요 😥
+					</h4>
+					<h2 className="text-Black typography-Body1 typography-M">
+						출금 계좌를 등록해주세요!
+					</h2>
 				</div>
 				<DefaultButton
 					title="출금 계좌 등록하기"
