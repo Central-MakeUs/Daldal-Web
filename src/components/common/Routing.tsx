0b9e71f2d58d0d77eecad1fe93e731bd32fc8e24
ShinYoung-Kim <lukey0515@naver.com>
2024-01-20 00:02:02 +0900

    feat: 라우팅 react router dom v6에 맞추어 수정

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -1,25 +1,34 @@
 import AccountBottomSheet from '@components/molecules/bottomSheet/AccountBottomSheet';
 import BankBottomSheet from '@components/molecules/bottomSheet/BankBottomSheet';
 import LoginBottomSheet from '@components/molecules/bottomSheet/LoginBottomSheet';
+import PostWithdrawalScreen from '@components/templates/Withdrawal/PostWithdrawalScreen';
+import PreWithdrawalScreen from '@components/templates/Withdrawal/PreWithdrawalScreen';
+import ProgressWithdrawalScreen from '@components/templates/Withdrawal/ProgressWithdrawalScreen';
 import Home from '@pages/Home';
 import ImageUpload from '@pages/ImageUpload';
 import MyPage from '@pages/MyPage';
 import Point from '@pages/Point';
-import Withdrawal from '@pages/Withdrawal';
 import { useModalStore } from '@stores/layerStore';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Routing = () => {
 	const { Modal } = useModalStore();
 
 	return (
 		<BrowserRouter>
-			<Route path="/" component={Home} exact />
-			<Route path="/home" component={Home} exact />
-			<Route path="/my-page" component={MyPage} exact />
-			<Route path="/point" component={Point} exact />
-			<Route path="/image-upload" component={ImageUpload} exact />
-			<Route path="/withdrawal" component={Withdrawal} exact />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/my-page" element={<MyPage />} />
+				<Route path="/point" element={<Point />} />
+				<Route path="/image-upload" element={<ImageUpload />} />
+				<Route path="/pre-withdrawal" element={<PreWithdrawalScreen />} />
+				<Route
+					path="/progress-withdrawal"
+					element={<ProgressWithdrawalScreen />}
+				/>
+				<Route path="/post-withdrawal" element={<PostWithdrawalScreen />} />
+			</Routes>
 			<LoginBottomSheet />
 			<AccountBottomSheet />
 			<BankBottomSheet />
