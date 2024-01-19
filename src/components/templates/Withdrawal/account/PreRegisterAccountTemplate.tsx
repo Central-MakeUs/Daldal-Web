c2aef2a3fb3080bc0f8bdcf1053aee3149a992c2
ShinYoung-Kim <lukey0515@naver.com>
2024-01-20 05:13:20 +0900

    feat: pre register account 템플릿 생성

---
+++ src/components/templates/Withdrawal/account/PreRegisterAccountTemplate.tsx
@@ -0,0 +1,32 @@
+import DefaultButton from '@components/atoms/button/DefaultButton';
+import FixedBottomLayout from '@layouts/FixedBottomLayout';
+import { mockImages } from '@mocks/images';
+
+const PreRegisterAccountTemplate = () => {
+	// todo: 디자이너님 이미지 생기면 mock 이미지 진짜 이미지로 변경
+	const image = mockImages[0];
+
+	return (
+		<div className="flex flex-col justify-center items-center h-[calc(100vh-105px)]">
+			<div className="flex flex-col gap-4 items-center text-center">
+				<img src={image} alt="step1" className="w-[163px] h-[163px]" />
+				<div className="typography-Body2 typography-R text-White">
+					등록된 계좌 정보가 없습니다.
+					<br />
+					계좌를 등록하여
+					<br />
+					환급액을 수령해보세요!
+				</div>
+			</div>
+			<FixedBottomLayout childrenPadding="px-6" height="h-15">
+				<DefaultButton
+					title="계좌 등록하기"
+					color={{ bgColor: 'White', textColor: 'Black' }}
+					size="large"
+				/>
+			</FixedBottomLayout>
+		</div>
+	);
+};
+
+export default PreRegisterAccountTemplate;
