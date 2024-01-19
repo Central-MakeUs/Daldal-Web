6c9060ebd58cbdf7ce459aedefba690a1ca9d2d9
ShinYoung-Kim <lukey0515@naver.com>
2024-01-20 04:03:57 +0900

    feat: category 페이지 params 변경

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,31 +1,27 @@
 import SearchTextField from '@components/atoms/SearchTextField';
 import SvgIcon from '@components/common/SvgIcon';
 import ProductCardList from '@components/organisms/ProductCardList';
-import categoryList from '@constants/categoryList';
+import categoryList, { CategoryName } from '@constants/categoryList';
 import colors from '@constants/colors';
 import PageLayout from '@layouts/PageLayout';
-import { IconId } from '@type/svgIcon';
-import { useRouteMatch } from 'react-router';
+import { useParams } from 'react-router-dom';
 
 type CategoryParams = {
-	id: 'clothes' | 'hat' | 'bags' | 'shoes' | 'jewlery';
+	categoryName: CategoryName;
 };
 
 const Category = () => {
-	const match = useRouteMatch<CategoryParams>();
-
-	const selected = match?.params.id === undefined ? 'clothes' : match.params.id;
-	const iconId = categoryList[selected].id as IconId;
-	const name = categoryList[selected].name;
+	const { categoryName = 'clothes' } = useParams<CategoryParams>();
+	const { id: iconId, name } = categoryList[categoryName];
 
 	return (
-		<PageLayout leftType="back" className="">
-			<SearchTextField className="!mx-6 !my-4" />
-			<div className="flex gap-1 mt-5 mb-3 mx-3">
+		<PageLayout leftType="back" className="p-3 relative">
+			<SearchTextField />
+			<div className="flex gap-1 mt-5 mb-3">
 				<SvgIcon id={iconId} size={32} color={colors.White} />
 				<h2 className="text-White typography-Subhead">{name}</h2>
 			</div>
-			<div className="px-[14.5px] py-3">
+			<div className="py-3">
 				<ProductCardList type="heart" />
 			</div>
 		</PageLayout>
