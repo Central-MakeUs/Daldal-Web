a940d8b294e91f2c38496d592cb79df4e6094805
ShinYoung-Kim <lukey0515@naver.com>
2024-01-20 00:00:17 +0900

    feat: ionic 설정 코드 제거

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
-import { setupIonicReact } from '@ionic/react';
 import { KonstaProvider } from 'konsta/react';
 
 import { GlobalStorybookStyles } from '../src/components/common/GlobalStorybookStyles';
 import { loadFontsForStorybook } from '../src/utils/loadFontsForStorybook';
 
 import 'tailwindcss/tailwind.css';
-import '@ionic/react/css/core.css';
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-setupIonicReact();
 loadFontsForStorybook();
 
 const withGlobalStyle = storyFn => (
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
 import CustomToaster from '@components/atoms/CustomToaster';
 import Routing from '@components/common/Routing';
-import { setupIonicReact } from '@ionic/react';
-
-import '@ionic/react/css/core.css';
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-setupIonicReact();
 
 const App = () => {
 	return (
