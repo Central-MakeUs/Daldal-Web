93092e0da423c6d522df76360815eb6c68ac5251
ShinYoung-Kim <lukey0515@naver.com>
2024-01-21 01:26:15 +0900

    feat: disabled account form 및 element 생성

---
+++ src/components/organisms/form/DisabledAccountForm.tsx
@@ -0,0 +1,23 @@
+import DisabledFormElement from '@components/molecules/form/DisabledFormElement';
+import { accountFormLabelMapping } from '@mocks/form';
+import { Account, AccountFormName } from '@models/form/entity/account';
+
+type DisabledAccountFormProps = {
+	defaultValues: Account;
+};
+
+const DisabledAccountForm = ({ defaultValues }: DisabledAccountFormProps) => {
+	return (
+		<div className="flex flex-col gap-7">
+			{Object.entries(defaultValues).map(([name, value], index) => (
+				<DisabledFormElement
+					key={index}
+					label={accountFormLabelMapping[name as AccountFormName]}
+					value={value}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default DisabledAccountForm;
