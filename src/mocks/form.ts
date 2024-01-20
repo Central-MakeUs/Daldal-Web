0032ddaeb9fbf86ec4ef1d2475cddf4c95205898
ShinYoung-Kim <lukey0515@naver.com>
2024-01-21 01:16:40 +0900

    feat: form mock 데이터 생성

---
+++ src/mocks/form.ts
@@ -0,0 +1,13 @@
+import { Account, AccountFormName } from '@models/form/entity/account';
+
+export const mockAccountForm: Account = {
+	USER: '홍길동',
+	BANK: '신한은행',
+	ACCOUNT: 110123456789,
+};
+
+export const accountFormLabelMapping: Record<AccountFormName, string> = {
+	USER: '입금자명',
+	BANK: '은행명',
+	ACCOUNT: '계좌번호',
+};
