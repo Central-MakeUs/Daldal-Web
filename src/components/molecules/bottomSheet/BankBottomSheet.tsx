5db36d86384a9692217a87cc4b1a0ba8a11f0ad4
ShinYoung-Kim <lukey0515@naver.com>
2024-01-21 01:26:34 +0900

    feat: bank bottom sheet에 전역 상태 관리로 연결 추가

--- src/components/molecules/bottomSheet/BankBottomSheet.tsx
+++ src/components/molecules/bottomSheet/BankBottomSheet.tsx
@@ -1,17 +1,26 @@
 import IconButton from '@components/atoms/button/IconButton';
 import BankButton from '@components/atoms/myPage/BankButton';
 import { Drawer, DrawerContent } from '@components/ui/drawer';
-import { bankList } from '@constants/bankList';
+import { BankNameType, bankList } from '@constants/bankList';
 import colors from '@constants/colors';
+import { useAccountInfoStore } from '@stores/formInfoStore';
 import { useBottomSheetStore } from '@stores/layerStore';
 
 const BankBottomSheet = () => {
 	const { type, isBottomSheetOpen, closeBottomSheet, setBottomSheetOpen } =
 		useBottomSheetStore();
 
+	const { setAccountInfo, accountInfo } = useAccountInfoStore();
+
+	const handleSelectBank = (bank: BankNameType) => {
+		setAccountInfo({ ...accountInfo, BANK: bank });
+		closeBottomSheet();
+	};
+
 	if (!isBottomSheetOpen || type !== 'bank') {
 		return null;
 	}
+
 	return (
 		<Drawer open={isBottomSheetOpen} onOpenChange={setBottomSheetOpen}>
 			<DrawerContent className="mx-auto w-full max-w-[600px] px-6 !h-[520px]">
@@ -28,11 +37,12 @@ const BankBottomSheet = () => {
 					은행을 선택해주세요.
 				</h2>
 				<div className="grid grid-cols-3 gap-[14px] overflow-auto mb-3">
-					{bankList.map(bank => (
+					{bankList.map((bank, index) => (
 						<BankButton
 							title={bank.title}
 							iconId={bank.iconId}
-							onClick={() => console.log('bank 클릭')}
+							onClick={() => handleSelectBank(bank.title)}
+							key={`BankButton#${index}`}
 						/>
 					))}
 				</div>
