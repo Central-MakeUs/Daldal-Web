8ef9321941e906b04eabda99b09a6d608417b9b1
ShinYoung-Kim <lukey0515@naver.com>
2024-01-21 02:23:09 +0900

    feat: enter type 추가

--- src/stores/formInfoStore.tsx
+++ src/stores/formInfoStore.tsx
@@ -1,12 +1,16 @@
 import { Account } from '@models/form/entity/account';
 import { create } from 'zustand';
 
+type EnterType = 'EDIT' | 'REGISTER';
+
 export type AccountInfoStoreType = {
 	accountInfo: Account;
 	setAccountInfo: (accountInfo: Account) => void;
 	isSelectedBankNeeded: boolean;
 	setIsSelectedBankNeeded: (isSelectedBankNeeded: boolean) => void;
 	clearAccountInfo: () => void;
+	enterType: 'EDIT' | 'REGISTER';
+	setEnterType: (enterType: 'EDIT' | 'REGISTER') => void;
 };
 
 export const useAccountInfoStore = create<AccountInfoStoreType>(set => ({
@@ -20,4 +24,6 @@ export const useAccountInfoStore = create<AccountInfoStoreType>(set => ({
 	setIsSelectedBankNeeded: (isSelectedBankNeeded: boolean) =>
 		set({ isSelectedBankNeeded }),
 	clearAccountInfo: () => set({ accountInfo: {} as Account }),
+	enterType: '' as EnterType,
+	setEnterType: (enterType: 'EDIT' | 'REGISTER') => set({ enterType }),
 }));
