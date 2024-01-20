35c0bd90336534a27d644e01b9b2df55312b5033
coddingyun <igy2840@gmail.com>
2024-01-20 23:07:42 +0900

    feat: 사진 업로드 홈 페이지 구현

--- src/pages/ImageUpload.tsx
+++ src/pages/ImageUpload.tsx
@@ -5,16 +5,28 @@ import { uploadImages } from '@mocks/uploadImages';
 
 const ImageUpload = () => {
 	return (
-		<PageLayout leftType="back" className="p-6 grid grid-cols-3 gap-[11px]">
-			<ImageUploadButton />
-			{uploadImages.map((image, idx) => (
-				<ImageContainer
-					key={`imageContainer${idx}`}
-					imageUrl={image.image}
-					status={image.tag}
-					onClick={() => console.log('test')}
-				/>
-			))}
+		<PageLayout leftType="back" className="px-6 py-3">
+			<div className="mb-3 flex flex-col gap-1 typography-Body1 typography-M">
+				<h1 className="text-White">사진은 꼭</h1>
+				<h1 className="text-Primary">제품명과 결제시간</h1>
+				<h1 className="text-White">이 나오게 업로드 부탁드립니다.</h1>
+			</div>
+			<p className="mb-6 typography-Body4 typography-R text-Gray20">
+				환급은 영업일 기준 48시간 이내에 검토되며, <br />
+				지그재그 내에서 '구매확정'을 누른 후, <br />
+				2주 이내 포인트가 환급됩니다.
+			</p>
+			<div className="grid grid-cols-3 gap-[11px]">
+				<ImageUploadButton />
+				{uploadImages.map((image, idx) => (
+					<ImageContainer
+						key={`imageContainer${idx}`}
+						imageUrl={image.image}
+						status={image.tag}
+						onClick={() => console.log('test')}
+					/>
+				))}
+			</div>
 		</PageLayout>
 	);
 };
