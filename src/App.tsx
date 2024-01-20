6f1443befd798c6278cb0b3f5f4135a347cea617
ShinYoung-Kim <lukey0515@naver.com>
2024-01-21 04:03:57 +0900

    chore: import 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import CustomToaster from '@components/atoms/CustomToaster';
 import Routing from '@components/common/Routing';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
 const App = () => {
 	return (
 		<>
