b033633a4a9495eedec9c7397af50c7439d1829a
ShinYoung-Kim <lukey0515@naver.com>
2024-01-21 16:12:00 +0900

    feat: 상품 detail 페이지 레이아웃 구성

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,60 +1,41 @@
-import ImageSlider from '@components/atoms/ImageSlider';
+import BuyWithLikeButton from '@components/atoms/BuyWithLikeButton';
+import CardInfo from '@components/atoms/card/CardInfo';
+import SmallProductDetailImageSlider from '@components/molecules/imageSlider/SmallProductDetailImageSlider';
+import FixedBottomLayout from '@layouts/FixedBottomLayout';
 import PageLayout from '@layouts/PageLayout';
 import { mockImages } from '@mocks/images';
+import YouTube from 'react-youtube';
 
 const ProductDetail = () => {
 	return (
 		<PageLayout leftType="back">
-			{/**회원가입 화면 */}
-			<ImageSlider
-				totalImageNumber={mockImages.length}
-				options={{ dots: true, autoplay: true, infinite: true }}
-				className="w-[287px]"
-			>
-				{mockImages.map((image, idx) => (
-					<div key={`Image#${idx}`}>
-						<div style={{ position: 'relative' }}>
-							<img src={image} alt="login image" className="aspect-square" />
-						</div>
-					</div>
-				))}
-			</ImageSlider>
-
-			{/**상세 페이지 작은 사진 화면 */}
-			<ImageSlider
-				totalImageNumber={mockImages.length}
-				className="w-screen max-w-[600px]"
-			>
-				{mockImages.map((image, idx) => (
-					<div key={`Image#${idx}`}>
-						<div style={{ position: 'relative' }}>
-							<img
-								src={image}
-								alt="detail small image"
-								className="w-full h-[calc(100vw*1.2)] max-h-[654px] object-cover"
-							/>
-						</div>
-					</div>
-				))}
-			</ImageSlider>
-
-			{/**상세 페이지 큰 사진 화면 */}
-			<ImageSlider
-				totalImageNumber={mockImages.length}
-				className="w-screen max-w-[600px]"
-			>
-				{mockImages.map((image, idx) => (
-					<div key={`Image#${idx}`}>
-						<div style={{ position: 'relative' }}>
-							<img
-								src={image}
-								alt="detail large image"
-								className="w-full h-screen"
-							/>
-						</div>
-					</div>
-				))}
-			</ImageSlider>
+			<SmallProductDetailImageSlider images={mockImages} />
+			<div className="p-5 flex flex-col gap-5">
+				<CardInfo
+					title="바토 볼레로 스퀘어넥 겨울 루즈핏 니트 (4color) 연말룩/데이트룩/하객룩"
+					price={19000}
+					paybackPrice={3000}
+					size="large"
+					category="의류"
+				/>
+				<div className="w-full aspect-video relative">
+					<YouTube
+						videoId="2g811Eo7K8U"
+						opts={{
+							height: '100%',
+							width: '100%',
+						}}
+						className="h-full w-full rounded-s overflow-hidden"
+					/>
+				</div>
+			</div>
+			<iframe
+				src="https://zigzag.kr/catalog/products/127482963"
+				className="w-full aspect-square"
+			/>
+			<FixedBottomLayout height={'h-[96px]'} childrenPadding={'bg-Gray90'}>
+				<BuyWithLikeButton like={false} />
+			</FixedBottomLayout>
 		</PageLayout>
 	);
 };
