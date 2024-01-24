80764e1ee161838f0de970dcbf832cfbba7946de
ShinYoung-Kim <lukey0515@naver.com>
2024-01-25 02:16:22 +0900

    feat: point list 변경

--- src/components/molecules/point/PointList.tsx
+++ src/components/molecules/point/PointList.tsx
@@ -1,48 +1,16 @@
 import { ListPoint } from '@components/atoms';
-import {
-	GROUP_ORDER_LIST_POINT_DESCRIPTION,
-	POINT_LIST_TITLE_BY_MONTH,
-	PointListTitleByMonthType,
-} from '@constants/text';
-import { Point } from '@models/point/entity/point';
-import { getPointText } from '@utils/formatData';
+import { PointHistoryList } from '@models/point/entity/point';
 
 type PointListProps = {
-	totalPoint: Point['point'];
-	pointList: Point[];
-	month: PointListTitleByMonthType;
+	pointList: PointHistoryList;
 };
 
-const PointList = ({ totalPoint, pointList, month }: PointListProps) => {
-	const descriptions = GROUP_ORDER_LIST_POINT_DESCRIPTION;
-	const isThisMonth = month === 'THIS';
-
+const PointList = ({ pointList }: PointListProps) => {
 	return (
-		<div className="flex flex-col gap-6 bg-Gray80 p-4 rounded-[13px]">
-			<div className="flex flex-col gap-3">
-				<span className="typography-Body2 typography-SB text-White">
-					{POINT_LIST_TITLE_BY_MONTH[month]}
-				</span>
-				<div className="flex flex-col typography-Body4 typography-R text-Gray20">
-					{isThisMonth &&
-						descriptions.map((description, index) => (
-							<span key={`PointListDescription#${index}`}>{description}</span>
-						))}
-				</div>
-			</div>
-			<span className="typography-Headline text-White self-end">
-				{getPointText(totalPoint)}
-			</span>
-			<div className="flex flex-col gap-2">
-				{pointList.map(({ point, date }, index) => (
-					<ListPoint
-						key={`ListPoint#${index}`}
-						point={point}
-						date={date}
-						onClick={() => console.log('포인트 정보 상세보기로 이동')}
-					/>
-				))}
-			</div>
+		<div className="flex flex-col gap-2">
+			{pointList.map((point, index) => (
+				<ListPoint key={`ListPoint#${index}`} {...point} />
+			))}
 		</div>
 	);
 };
