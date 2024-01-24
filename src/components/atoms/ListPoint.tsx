47ea05d177de56145de894ab5444fea6d6b64197
ShinYoung-Kim <lukey0515@naver.com>
2024-01-25 02:15:03 +0900

    feat: list point 컴포넌트 변경

--- src/components/atoms/ListPoint.tsx
+++ src/components/atoms/ListPoint.tsx
@@ -2,47 +2,45 @@ import { Block, Button } from 'konsta/react';
 
 import { SvgIcon } from '@components/common';
 import colors from '@constants/colors';
-import { Point } from '@models/point/entity/point';
-import {
-	getDataInYYYYMMDDSplitedByDot,
-	getPointText,
-	isPointState,
-} from '@utils/formatData';
+import { PointHistory, RefundStatus } from '@models/point/entity/point';
+import { getDataInYYYYMMDDSplitedByDot, getPointText } from '@utils/formatData';
+import { useNavigate } from 'react-router-dom';
 
-type ListPointProps = {
-	onClick: () => void;
-} & Point;
+type ListPointProps = PointHistory;
 
-const ListPoint = ({ date, point, onClick }: ListPointProps) => {
-	const colorsByPoint: { [key: string]: string } = {
-		미승인: 'text-Error',
-		진행중: 'text-Secondary_B',
-		point: 'text-Gray10',
+const ListPoint = ({
+	uploadTime,
+	refund,
+	refundStatus,
+	buyId,
+}: ListPointProps) => {
+	const navigate = useNavigate();
+	const handleNavigateToDetailPage = () => {
+		navigate(`/image-upload/${buyId}`);
 	};
 
-	const getColorByPoint = () => {
-		if (!isPointState(point)) {
-			return colorsByPoint.point;
-		}
-
-		return colorsByPoint[point];
+	const colorsByPoint: { [key in RefundStatus]: string } = {
+		미승인: 'text-Error',
+		진행중: 'text-Secondary_B',
+		승인: 'text-Gray20',
+		출금: 'text-Gray20',
 	};
 
 	return (
 		<Button
 			className="flex items-center rounded-[13px] !p-4 !k-color-Gray90 h-[66px]"
-			onClick={onClick}
+			onClick={handleNavigateToDetailPage}
 		>
 			<Block className="flex items-center flex-1 gap-[10px] !m-0 !p-0">
 				<SvgIcon id="point-paid" color={colors.Gray20} size={24} />
 				<Block className="flex flex-col !m-0 !p-0 text-left">
 					<span className="typography-Caption1 typography-SB text-Gray50">
-						{getDataInYYYYMMDDSplitedByDot(date)}
+						{getDataInYYYYMMDDSplitedByDot(uploadTime)}
 					</span>
 					<span
-						className={`typography-Body2 typography-R ${getColorByPoint()}`}
+						className={`typography-Body2 typography-R ${colorsByPoint[refundStatus]}`}
 					>
-						{getPointText(point)}
+						{getPointText(refund, refundStatus)}
 					</span>
 				</Block>
 			</Block>
