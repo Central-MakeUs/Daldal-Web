10d928b0f7b41be5e26fed38aefdc453792a8b5a
ShinYoung-Kim <lukey0515@naver.com>
2024-01-25 02:14:13 +0900

    feat: 타입 수정

--- src/models/point/entity/point.ts
+++ src/models/point/entity/point.ts
@@ -1,4 +1,11 @@
-export type Point = {
-	point: string;
-	date: string;
+export type Point = string;
+export type RefundStatus = '진행중' | '미승인' | '승인' | '출금';
+
+export type PointHistory = {
+	refund?: Point;
+	uploadTime: string;
+	buyId: number;
+	refundStatus: RefundStatus;
 };
+
+export type PointHistoryList = PointHistory[];
