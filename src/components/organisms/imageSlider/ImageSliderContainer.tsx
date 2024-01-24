094b51779a68fb2d6cf60b0997bc68c5e1528d31
ShinYoung-Kim <lukey0515@naver.com>
2024-01-24 11:48:10 +0900

    feat: detail 이미지 슬라이더 닫기 아이콘 변경

--- src/components/organisms/imageSlider/ImageSliderContainer.tsx
+++ src/components/organisms/imageSlider/ImageSliderContainer.tsx
@@ -1,6 +1,5 @@
 import { IconButton } from '@components/atoms';
 import { LargeProductDetailImageSlider } from '@components/molecules';
-import colors from '@constants/colors';
 
 type ImageSliderContainerProps = {
 	images: string[];
@@ -12,13 +11,12 @@ const ImageSliderContainer = ({
 	handleClose,
 }: ImageSliderContainerProps) => {
 	return (
-		<div className="w-screen h-screen top-0 left-0 flex items-start justify-center overflow-hidden bg-Gray60">
+		<div className="w-screen h-screen top-0 left-0 flex items-start justify-center overflow-hidden bg-Black">
 			<LargeProductDetailImageSlider images={images} />
 			<div className="absolute top-6 left-4 z-20">
 				<IconButton
 					icon={{
-						id: 'close',
-						color: colors.Black,
+						id: 'circle-cancel',
 						size: 24,
 					}}
 					onClick={handleClose}
