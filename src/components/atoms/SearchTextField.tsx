e43274d67bf4abdd9bf63e292ecf406ffac94bcf
ShinYoung-Kim <lukey0515@naver.com>
2024-01-25 11:28:06 +0900

    feat: placeholder text 변경

--- src/components/atoms/SearchTextField.tsx
+++ src/components/atoms/SearchTextField.tsx
@@ -29,7 +29,7 @@ const SearchTextField = ({ isFocused = false }: SearchTextFieldProps) => {
 				onChange={e => setSearchValue(e.target.value)}
 				onKeyDown={handleEnter}
 				type="text"
-				placeholder="Search"
+				placeholder="화장품 사고 3% 환급받자💄"
 				className="bg-Gray80 text-white placeholder:text-Gray20 flex-1 focus-visible:outline-none typography-Body2 typography-R"
 			/>
 		</div>
