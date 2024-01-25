c5d6d9d6760b959f509654d59056472ab5c2b60d
ShinYoung-Kim <lukey0515@naver.com>
2024-01-26 00:13:26 +0900

    feat: search list container 생성

---
+++ src/components/organisms/RecentSearchListContainer.tsx
@@ -0,0 +1,12 @@
+import { SearchHeader, RecentSearchList } from '@components/molecules';
+
+const RecentSearchListContainer = () => {
+	return (
+		<div className="px-1">
+			<SearchHeader />
+			<RecentSearchList />
+		</div>
+	);
+};
+
+export default RecentSearchListContainer;
