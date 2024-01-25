32af4df92af6131d84c59f30c6fa44f5837e06a8
ShinYoung-Kim <lukey0515@naver.com>
2024-01-26 04:34:45 +0900

    feat: 모달 버튼 외부에서 주입

--- src/components/atoms/button/RequestLeaveModalButton.tsx
+++ src/components/atoms/button/RequestLeaveModalButton.tsx
@@ -1,12 +1,43 @@
+import { DefaultButton } from '@components/atoms';
 import { useModalStore } from '@stores/layerStore';
+import { useNavigate } from 'react-router-dom';
 
 const RequestLeaveModalButton = () => {
 	const { openModal } = useModalStore();
+	const navigate = useNavigate();
+
+	const goBack = () => {
+		navigate(-1);
+	};
+	const stay = () => {};
+
+	const buttons = [
+		<DefaultButton
+			color={{
+				textColor: 'White',
+				bgColor: 'Primary',
+			}}
+			title="머무르기"
+			size="small"
+			onClick={stay}
+		/>,
+		<DefaultButton
+			title="이동하기"
+			size="small"
+			color={{
+				textColor: 'Primary',
+				bgColor: 'White',
+				borderColor: 'Primary',
+			}}
+			onClick={goBack}
+		/>,
+	];
+
 	const handleClickRequestLeaveModalButton = (
 		e: React.MouseEvent<HTMLDivElement, MouseEvent>,
 	) => {
 		e.preventDefault();
-		openModal('leave');
+		openModal('leave', buttons);
 	};
 
 	return (
