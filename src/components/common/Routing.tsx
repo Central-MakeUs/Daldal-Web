5f3e740ac995848d96f573103bc75e1785359e21
ShinYoung-Kim <lukey0515@naver.com>
2024-01-26 01:04:43 +0900

    feat: search result 라우팅 추가

--- src/components/common/Routing.tsx
+++ src/components/common/Routing.tsx
@@ -22,6 +22,7 @@ import {
 	ReadAccountInfo,
 	SaveAccountInfo,
 	Search,
+	SearchResult,
 	Withdrawal,
 } from '@pages/index';
 import { useModalStore } from '@stores/layerStore';
@@ -54,6 +55,7 @@ const Routing = () => {
 				<Route path="/post-account" element={<SaveAccountInfo />} />
 				<Route path="/detail/:productId" element={<ProductDetail />} />
 				<Route path="/search" element={<Search />} />
+				<Route path="/search/:searchQuery" element={<SearchResult />} />
 			</Routes>
 			<LoginBottomSheet />
 			<AccountBottomSheet />
