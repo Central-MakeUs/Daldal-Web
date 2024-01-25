759e14f0198cd8f8baeaba075725965c1d08094e
ShinYoung-Kim <lukey0515@naver.com>
2024-01-26 03:06:45 +0900

    feat: 검색어 클릭 시 이동 구현

--- src/components/molecules/search/RecentSearchList.tsx
+++ src/components/molecules/search/RecentSearchList.tsx
@@ -1,12 +1,24 @@
 import { SearchTag } from '@components/atoms';
 import { useSearchHistoryStore } from '@stores/searchHistoryStore';
+import { SearchQuery } from '@type/searchQuery';
+import { useNavigate } from 'react-router-dom';
 
 const RecentSearchList = () => {
 	const searchHistory = useSearchHistoryStore(state => state.searchHistoryList);
+	const navigate = useNavigate();
+
+	const handleSearchTagClick = (searchQuery: SearchQuery) => {
+		navigate(`/search/${searchQuery}`);
+	};
+
 	return (
 		<div className="w-full flex flex-wrap gap-2">
 			{searchHistory.map((searchQuery, index) => (
-				<div className="py-3" key={`SearchQuery#${index}`}>
+				<div
+					className="py-3 cursor-pointer"
+					key={`SearchQuery#${index}`}
+					onClick={() => handleSearchTagClick(searchQuery)}
+				>
 					<SearchTag searchQuery={searchQuery} />
 				</div>
 			))}
