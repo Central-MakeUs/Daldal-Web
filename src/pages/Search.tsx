38772fa9c578d524b25c9f48822d21239fc67508
ShinYoung-Kim <lukey0515@naver.com>
2024-01-26 00:13:40 +0900

    feat: search 페이지 생성

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,14 @@
+import { SearchTextField } from '@components/atoms';
+import RecentSearchListContainer from '@components/organisms/RecentSearchListContainer';
+import PageLayout from '@layouts/PageLayout';
+
 const Search = () => {
-	return <div>search</div>;
+	return (
+		<PageLayout className="px-3" leftType="back">
+			<SearchTextField isFocused />
+			<RecentSearchListContainer />
+		</PageLayout>
+	);
 };
 
 export default Search;
