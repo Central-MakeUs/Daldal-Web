65dfc151c4087743b69b11ae1185ac7ce4f066fd
ShinYoung-Kim <lukey0515@naver.com>
2024-01-26 04:17:58 +0900

    feat: leave 모달 띄우는 버튼 생성

---
+++ src/components/atoms/button/RequestLeaveModalButton.tsx
@@ -0,0 +1,20 @@
+import { useModalStore } from '@stores/layerStore';
+
+const RequestLeaveModalButton = () => {
+	const { openModal } = useModalStore();
+	const handleClickRequestLeaveModalButton = (
+		e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+	) => {
+		e.preventDefault();
+		openModal('leave');
+	};
+
+	return (
+		<div
+			className="w-6 h-6 cursor-pointer py-4 bg-blue-700 fixed top-[14px] z-30"
+			onClick={handleClickRequestLeaveModalButton}
+		/>
+	);
+};
+
+export default RequestLeaveModalButton;
