24948b61ca970b54b0c6f63ebec420d748144d94
coddingyun <igy2840@gmail.com>
2024-01-27 23:27:06 +0900

    feat: ProgressBar bgcolor 설정

--- src/components/atoms/BarContainer.tsx
+++ src/components/atoms/BarContainer.tsx
@@ -2,16 +2,15 @@
 
 const BarContainer = ({ animationDuration, children, isFinished }: any) => (
 	<div
-		className={`relative bg-Gray20 ${
+		className={`relative ${
 			isFinished ? 'opacity-0' : 'opacity-100'
 		} pointer-events-none`}
 		style={{
 			transition: `opacity ${animationDuration}ms linear`,
 		}}
 	>
-		{/* <div className="relative max-w-[600px]"> */}
+		<div className="bg-Gray10 w-full fixed top-0 left-0 h-1 z-40" />
 		{children}
-		{/* </div> */}
 	</div>
 );
 
