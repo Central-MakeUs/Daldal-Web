cb88a52f47d697a84d0750e0fb1c1fa7fd1fcf8f
coddingyun <igy2840@gmail.com>
2024-01-28 16:45:10 +0900

    refactor: 필요없는 속성 제거

--- src/components/atoms/BarContainer.tsx
+++ src/components/atoms/BarContainer.tsx
@@ -1,25 +1,16 @@
 import { ReactNode } from 'react';
 
 type BarContainerProps = {
-	animationDuration: number;
 	children: ReactNode;
 	isFinished: boolean;
 };
 
-const BarContainer = ({
-	animationDuration,
-	children,
-	isFinished,
-}: BarContainerProps) => (
+const BarContainer = ({ children, isFinished }: BarContainerProps) => (
 	<div
 		className={`relative ${
 			isFinished ? 'opacity-0' : 'opacity-100'
 		} pointer-events-none`}
-		style={{
-			transition: `opacity ${animationDuration}ms linear`,
-		}}
 	>
-		<div className="bg-Gray10 w-full fixed top-0 left-0 h-1 z-40" />
 		{children}
 	</div>
 );
