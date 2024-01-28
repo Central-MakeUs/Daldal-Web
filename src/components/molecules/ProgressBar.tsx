a890cbf543a96f696a8db66b582cb8bc89bbb439
coddingyun <igy2840@gmail.com>
2024-01-28 16:45:34 +0900

    refactor: prop 제거

--- src/components/molecules/ProgressBar.tsx
+++ src/components/molecules/ProgressBar.tsx
@@ -9,8 +9,9 @@ const ProgressBar = ({ isAnimating }: ProgressBarProps) => {
 	const { animationDuration, progress, isFinished } = useNProgress({
 		isAnimating,
 	});
+
 	return (
-		<BarContainer animationDuration={animationDuration} isFinished={isFinished}>
+		<BarContainer isFinished={isFinished}>
 			<Bar animationDuration={animationDuration} progress={progress} />
 		</BarContainer>
 	);
