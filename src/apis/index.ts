0d0eb5a74c78c3d9cec616191dd8ad79c3e878e7
coddingyun <igy2840@gmail.com>
2024-02-02 11:23:15 +0900

    feat: api instance 구조 완성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,8 @@
-import axios, { AxiosError } from 'axios';
+import axios, {
+	AxiosError,
+	AxiosResponse,
+	InternalAxiosRequestConfig,
+} from 'axios';
 
 import { getAccessToken } from '@utils/token';
 
@@ -22,18 +26,28 @@ export const api = axios.create({
 	withCredentials: true,
 });
 
-api.interceptors.request.use(
-	async config => {
-		const accessToken = getAccessToken();
+const onRequestFulfilled = async (config: InternalAxiosRequestConfig) => {
+	const accessToken = getAccessToken();
 
-		if (accessToken) {
-			config.headers.Authorization = accessToken;
-		}
+	if (accessToken && config.headers) {
+		config.headers['Authorization'] = accessToken;
+	}
 
-		return config;
-	},
-	(error: AxiosError) => {
-		console.log('interceptor > error', error);
-		Promise.reject(error);
-	},
-);
+	return config;
+};
+
+const onRequestRejected = (error: AxiosError) => {
+	console.log('interceptor > error', error);
+	Promise.reject(error);
+};
+
+const onResponseFulfilled = (response: AxiosResponse) => response;
+
+const onResponseRejected = async (error: AxiosError) => {
+	if (error.response?.status === 401) {
+		// TODO: refresh token 으로 access token 받아오기
+	}
+};
+
+api.interceptors.request.use(onRequestFulfilled, onRequestRejected);
+api.interceptors.response.use(onResponseFulfilled, onResponseRejected);
