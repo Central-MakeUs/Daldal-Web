7f96cdaf85bf49132b7f78168bc1e9b3c2b662af
coddingyun <igy2840@gmail.com>
2024-02-03 01:15:22 +0900

    feat: status 여부에 따른 WishList 페이지 구현 완료

--- src/pages/WishList.tsx
+++ src/pages/WishList.tsx
@@ -1,9 +1,30 @@
+import {
+	DefaultWishList,
+	DefaultWishListWithNoItem,
+	EditWishList,
+} from '@components/templates';
 import PageLayout from '@layouts/PageLayout';
+import { mockProductSimpleList } from '@mocks/mockProductSimpleList';
+import { useWishListStore } from '@stores/wishListStore';
 
 const WishList = () => {
+	const wishListStatus = useWishListStore(state => state.wishListStatus);
+
+	const mockData = mockProductSimpleList;
+
+	const renderContent = () => {
+		if (wishListStatus === 'default') {
+			if (mockData.length > 0) {
+				return <DefaultWishList productList={mockData} />;
+			}
+			return <DefaultWishListWithNoItem />;
+		}
+
+		return <EditWishList />;
+	};
 	return (
-		<PageLayout leftType="home">
-			<div>찜한 목록</div>
+		<PageLayout leftType="home" className="h-full flex flex-col px-3 py-3">
+			{renderContent()}
 		</PageLayout>
 	);
 };
