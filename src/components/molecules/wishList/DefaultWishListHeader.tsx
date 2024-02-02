1cb5870926506b03e4f6604893a55a212e40dd47
coddingyun <igy2840@gmail.com>
2024-02-03 01:16:02 +0900

    feat: Header에서 status 변경 기능 추가

--- src/components/molecules/wishList/DefaultWishListHeader.tsx
+++ src/components/molecules/wishList/DefaultWishListHeader.tsx
@@ -1,12 +1,21 @@
 import { Button } from 'konsta/react';
 
+import { useWishListStore } from '@stores/wishListStore';
+
 const DefaultWishListHeader = () => {
+	const setWishListStatus = useWishListStore(state => state.setWishListStatus);
+
+	const handleEdit = () => {
+		setWishListStatus('edit');
+	};
+
 	return (
 		<div className="px-3 flex justify-between items-center">
 			<div className="typography-Body1 typography-M text-White">찜한 목록</div>
 			<Button
 				className="!w-auto !typography-Body4 !typography-R k-color-Secondary_B"
 				clear
+				onClick={handleEdit}
 			>
 				편집하기
 			</Button>
--- src/components/molecules/wishList/EditWishListHeader.tsx
+++ src/components/molecules/wishList/EditWishListHeader.tsx
@@ -1,10 +1,21 @@
 import { Button } from 'konsta/react';
 
+import { useWishListStore } from '@stores/wishListStore';
+
 const EditWishListHeader = () => {
+	const setWishListStatus = useWishListStore(state => state.setWishListStatus);
+
+	const handleComplete = () => {
+		setWishListStatus('default');
+	};
+
 	return (
 		<div className="px-3 flex justify-between items-center">
 			<div className="typography-Body1 typography-M text-White">상품 편집</div>
-			<Button className="!w-auto !typography-Body4 !typography-R k-color-Secondary_B !text-Black rounded-[38px] !px-3 !py-1">
+			<Button
+				className="!w-auto !typography-Body4 !typography-R k-color-Secondary_B !text-Black rounded-[38px] !px-3 !py-1"
+				onClick={handleComplete}
+			>
 				완료
 			</Button>
 		</div>
