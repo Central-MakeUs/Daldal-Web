f7e7804ac1595de61612ad2b1792441cddc73207
coddingyun <igy2840@gmail.com>
2024-02-02 11:27:45 +0900

    fix: buyId 복구

--- src/models/point/entity/point.ts
+++ src/models/point/entity/point.ts
@@ -4,7 +4,7 @@ export type RefundStatus = '진행중' | '미승인' | '승인' | '출금';
 export type PointHistory = {
 	refund?: Point;
 	uploadTime: string;
-	detailId: number;
+	buyId: number;
 	refundStatus: RefundStatus;
 };
 
