6c833c1599cc5db94bdfc4bf7b606543c04ba421
coddingyun <igy2840@gmail.com>
2024-02-05 21:23:21 +0900

    feat: RecomendationProductCardList api 연동

--- src/components/organisms/RecommendationProductCardList.tsx
+++ src/components/organisms/RecommendationProductCardList.tsx
@@ -1,9 +1,14 @@
 import { ProductCardList } from '@components/organisms';
-import { mockProductSimpleList } from '@mocks/mockProductSimpleList';
+import { useGetProductSimpleList } from '@hooks/apis/product';
+//import { mockProductSimpleList } from '@mocks/mockProductSimpleList';
 
 const RecommendationProductCardList = () => {
 	// TODO: 추천상품 API 연동
-	const productList = mockProductSimpleList.slice(0, 3);
+	const { data } = useGetProductSimpleList();
+
+	//console.log(data?.pages[0].data);
+
+	//const productList = mockProductSimpleList.slice(0, 3);
 
 	return (
 		<div className="flex flex-col gap-3">
@@ -14,7 +19,7 @@ const RecommendationProductCardList = () => {
 				gridCols={3}
 				type="heart"
 				size="small"
-				productList={productList}
+				productList={data?.pages[0].data}
 			/>
 		</div>
 	);
