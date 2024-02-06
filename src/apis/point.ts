39987aeee16534ec509b30edc2f8221dfb121a36
coddingyun <igy2840@gmail.com>
2024-02-06 21:13:16 +0900

    fix: type 오류 수정

--- src/apis/point.ts
+++ src/apis/point.ts
@@ -1,10 +1,10 @@
-import { ProductDetailListResponseDTO } from '@models/product/response/productDetailListResponseDTO';
+import { CurrentPointResponseDTO } from '@models/point/response/currentPointResponseDTO';
 import { ApiResponse } from '@type/apiResponse';
 
 import { api } from '.';
 
 export const getCurrentPoint = async (): Promise<
-	ApiResponse<ProductDetailListResponseDTO>
+	ApiResponse<CurrentPointResponseDTO>
 > => {
 	const response = await api.get('/api/v1/points/me');
 	return response.data;
