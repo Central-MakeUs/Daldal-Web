c9c404e44fe696c764022dc62d71823bec294687
coddingyun <igy2840@gmail.com>
2024-02-08 09:40:38 +0900

    feat: accessToken, refreshToken 추출

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { SearchTextField } from '@components/atoms';
 import { CategoryButtonList } from '@components/molecules';
 import {
@@ -6,11 +8,25 @@ import {
 } from '@components/organisms';
 import { useGetProductSimpleList } from '@hooks/apis/product';
 import PageLayout from '@layouts/PageLayout';
+import { setAccessToken, setRefreshToken } from '@utils/localStorage/token';
+import { useSearchParams } from 'react-router-dom';
 
 const Home = () => {
 	const { data } = useGetProductSimpleList();
 
 	//TODO params access/refresh token 받아오기
+	const [searchParams] = useSearchParams();
+	const accessTokenParam = searchParams.get('access-token');
+	const refreshTokenParam = searchParams.get('refresh-token');
+
+	useEffect(() => {
+		if (accessTokenParam) {
+			setAccessToken(accessTokenParam);
+		}
+		if (refreshTokenParam) {
+			setRefreshToken(refreshTokenParam);
+		}
+	}, []);
 
 	return (
 		<PageLayout leftType="logo" className="flex flex-col">
