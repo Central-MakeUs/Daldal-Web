64975b1a31ca01999d96996200ad0529107800b2
coddingyun <igy2840@gmail.com>
2024-02-08 15:40:09 +0900

    refactor: 로그인 api 제거

--- src/hooks/apis/auth.ts
+++ src/hooks/apis/auth.ts
@@ -1,13 +1,9 @@
-import { getUserInfo, kakaoLogin, logout } from '@apis/auth';
+import { getUserInfo, logout } from '@apis/auth';
 import { LogoutRequestDTO } from '@models/auth/request/logoutRequestDTO';
-import { TokenResponseDTO } from '@models/auth/response/tokenResponseDTO';
 import { useMutation } from '@tanstack/react-query';
-import { ApiResponse } from '@type/apiResponse';
 import {
 	removeAccessToken,
 	removeRefreshToken,
-	setAccessToken,
-	setRefreshToken,
 } from '@utils/localStorage/token';
 import {
 	removeProvider,
@@ -15,21 +11,6 @@ import {
 	setProvider,
 	setUserName,
 } from '@utils/localStorage/userInfo';
-
-export const useKakaoLogin = (errorCallback?: (error: Error) => void) => {
-	return useMutation({
-		mutationFn: () => kakaoLogin(),
-		onSuccess: async (data: ApiResponse<TokenResponseDTO>) => {
-			setAccessToken(data.data.accessToken);
-			setRefreshToken(data.data.refreshToken);
-
-			const userInfoData = await getUserInfo();
-			setUserName(userInfoData.data.name);
-			setProvider(userInfoData.data.provider === 'KAKAO' ? '카카오톡' : '애플');
-		},
-		onError: errorCallback,
-	});
-};
 
 export const useLogout = (
 	successCallback: () => void,
