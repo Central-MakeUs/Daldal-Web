927becaf184dfe80ca19541a47d09dd1617d99a2
coddingyun <igy2840@gmail.com>
2024-02-09 20:02:36 +0900

    feat: 찜 페이지 개수 섹션 추가

--- src/components/templates/wishList/DefaultWishList.tsx
+++ src/components/templates/wishList/DefaultWishList.tsx
@@ -4,13 +4,21 @@ import { ProductSimpleList } from '@models/product/entity/product';
 
 type DefaultWishListProps = {
 	productList: ProductSimpleList;
+	totalProductLength: number;
 };
 
-const DefaultWishList = ({ productList }: DefaultWishListProps) => {
+const DefaultWishList = ({
+	productList,
+	totalProductLength,
+}: DefaultWishListProps) => {
 	return (
 		<>
 			<DefaultWishListHeader />
 			<div className="my-3">
+				<h4 className="typography-Body4 typography-R text-White">
+					검색 결과 <span className="text-Primary">{totalProductLength}</span>{' '}
+					개
+				</h4>
 				<ProductCardList type="default" productList={productList} />
 			</div>
 		</>
