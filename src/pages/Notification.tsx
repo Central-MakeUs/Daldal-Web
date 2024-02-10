fe6f52849d1cd7d649d391181cc5e808d46e05aa
coddingyun <igy2840@gmail.com>
2024-02-10 19:48:00 +0900

    feat: Notification Page description 추가

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -36,6 +36,9 @@ const Notification = () => {
 		{ message: '환급액을 받아가세요!', style: 'text-Gray60' },
 	];
 
+	const subDescription =
+		'* 캡처본은 제품명과 결제 시간이 포함되어야합니다.\n* 기재 된 링크를 통해 발생한 수익의 일부를 받을 수 있습니다.';
+
 	return (
 		<ClearLayout className="flex flex-col relative bg-White py-[31px]">
 			<ProgressBar isAnimating={state.isAnimating} key={state.key} />
@@ -47,8 +50,8 @@ const Notification = () => {
 					{desc.message}
 				</h3>
 			))}
-			<h4 className="typography-Body4 typography-M text-Error mt-2">
-				* 캡처본은 제품명과 결제 시간이 포함되어야합니다.
+			<h4 className="typography-Body4 typography-M text-Error mt-2 whitespace-pre-line">
+				{subDescription}
 			</h4>
 			<div className="flex-grow h-full flex items-center justify-center">
 				<img src={notificationImg} width={314} height={398} />
