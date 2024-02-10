7cdce5cee5c0830616ae396be1ec532598bbcdad
coddingyun <igy2840@gmail.com>
2024-02-10 16:06:54 +0900

    design: my page 이메일 폰트 디자인 변경

--- src/utils/localStorage/userInfo.ts
+++ src/utils/localStorage/userInfo.ts
@@ -3,12 +3,12 @@ import LocalStorageKey from '@constants/localStorageKey';
 export const getUserName = () =>
 	localStorage.getItem(LocalStorageKey.USER_NAME);
 export const setUserName = (userName: string) =>
-	localStorage.setItem(LocalStorageKey.USER_NAME, JSON.stringify(userName));
+	localStorage.setItem(LocalStorageKey.USER_NAME, userName);
 export const removeUserName = () =>
 	localStorage.removeItem(LocalStorageKey.USER_NAME);
 
 export const getProvider = () => localStorage.getItem(LocalStorageKey.PROVIDER);
 export const setProvider = (provider: string) =>
-	localStorage.setItem(LocalStorageKey.PROVIDER, JSON.stringify(provider));
+	localStorage.setItem(LocalStorageKey.PROVIDER, provider);
 export const removeProvider = () =>
 	localStorage.removeItem(LocalStorageKey.PROVIDER);
