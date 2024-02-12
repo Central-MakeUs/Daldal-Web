59f64d2b8d48388da16595fefa67141c0696e997
ShinYoung-Kim <lukey0515@naver.com>
2024-02-12 11:22:48 +0900

    feat: verify token, renew access token 함수 구현

--- src/utils/localStorage/token.ts
+++ src/utils/localStorage/token.ts
@@ -1,4 +1,5 @@
 import LocalStorageKey from '@constants/localStorageKey';
+import { removeServiceAccountCache } from '@utils/localStorage/removeServiceAccountCache';
 
 export const getAccessToken = () =>
 	localStorage.getItem(LocalStorageKey.ACCESS_TOKEN);
@@ -13,3 +14,37 @@ export const setRefreshToken = (refreshToken: string) =>
 	localStorage.setItem(LocalStorageKey.REFRESH_TOKEN, refreshToken);
 export const removeRefreshToken = () =>
 	localStorage.removeItem(LocalStorageKey.REFRESH_TOKEN);
+
+const parseJwt = (token: string) => {
+	return JSON.parse(atob(token.split('.')[1]));
+};
+
+export const verifyToken = (token: string) => {
+	const parsedToken = parseJwt(token);
+	const exp = parsedToken.exp;
+	const now = new Date().getTime() / 1000;
+	return now < exp;
+};
+
+export const verifyAccessToken = () => {
+	const accessToken = getAccessToken();
+	if (accessToken) {
+		return verifyToken(accessToken);
+	}
+
+	return false;
+};
+
+export const verifyRefreshToken = () => {
+	const refreshToken = getRefreshToken();
+	if (refreshToken) {
+		return verifyToken(refreshToken);
+	}
+
+	return false;
+};
+
+export const renewAccessToken = () => {
+	removeServiceAccountCache();
+	window.location.href = '/sign-up';
+};
