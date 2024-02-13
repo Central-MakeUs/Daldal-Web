b115fe8ea96901995a5ef62a40f6af5438ba62ed
ShinYoung-Kim <lukey0515@naver.com>
2024-02-13 19:29:36 +0900

    feat: 카테고리 페이지 무한스크롤 적용

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,9 +1,12 @@
+import { Preloader } from 'konsta/react';
+
 import { SearchTextField } from '@components/atoms';
 import { SvgIcon } from '@components/common';
 import { ProductCardList } from '@components/organisms';
 import categoryList, { CategoryName } from '@constants/categoryList';
 import colors from '@constants/colors';
 import { useGetProductSimpleList } from '@hooks/apis/product';
+import useIntersection from '@hooks/infiniteScroll';
 import PageLayout from '@layouts/PageLayout';
 import { useParams } from 'react-router-dom';
 
@@ -14,8 +17,9 @@ type CategoryParams = {
 const Category = () => {
 	const { categoryName = 'TOPS' } = useParams<CategoryParams>();
 	const { id: iconId, name } = categoryList[categoryName];
-
-	const { data } = useGetProductSimpleList(categoryName);
+	const { data, fetchNextPage, isFetchingNextPage } =
+		useGetProductSimpleList(categoryName);
+	const intersectionRef = useIntersection(fetchNextPage);
 
 	return (
 		<PageLayout leftType="back" className="px-3 relative flex flex-col">
@@ -25,11 +29,20 @@ const Category = () => {
 				<h2 className="text-White typography-Subhead">{name}</h2>
 			</div>
 			<div className="py-3">
-				<ProductCardList
-					type="heart"
-					productList={data?.pages[0].data.itemResponses}
-				/>
+				{data?.pages.map((page, index) => (
+					<ProductCardList
+						key={`ProductCardList#${index}`}
+						productList={page.data.itemResponses}
+						type="heart"
+					/>
+				))}
 			</div>
+			<div ref={intersectionRef} className="w-full h-6" />
+			{isFetchingNextPage && (
+				<div className="w-full h-full flex justify-center items-center">
+					<Preloader className="k-color-Primary" />
+				</div>
+			)}
 		</PageLayout>
 	);
 };
