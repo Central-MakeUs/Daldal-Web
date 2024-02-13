4eaadbf69b58be0f196b8456f0559516875ae8d4
ShinYoung-Kim <lukey0515@naver.com>
2024-02-14 00:07:32 +0900

    fix: super token 없는 경우 상품 detail 조회 오류 해결

--- src/hooks/apis/account.ts
+++ src/hooks/apis/account.ts
@@ -1,8 +1,23 @@
 import { getAccountInfo, patchAccountInfo } from '@apis/account';
 import { AccountInfoRequestDTO } from '@models/account/request/accountInfoRequestDTO';
 import { useMutation, useQuery } from '@tanstack/react-query';
+import { getAccessToken } from '@utils/localStorage/token';
 
 export const useGetAccountInfo = () => {
+	const accessToken = getAccessToken();
+
+	if (!accessToken) {
+		return {
+			data: {
+				account: '',
+				accountBank: '',
+				depositorName: '',
+			},
+			isError: true,
+		};
+	}
+
+	// eslint-disable-next-line react-hooks/rules-of-hooks
 	return useQuery({
 		queryKey: ['accountInfo'],
 		queryFn: () => getAccountInfo(),
